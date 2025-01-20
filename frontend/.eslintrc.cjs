661e99f91afe050416b8f4e4e696637dd2804bb5
Efren Lim <elim@linuxfoundation.org>
2025-01-20 10:45:38 +0800

    initial frontend structure
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,6 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-airbnb'],
+};
---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Sample Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const test = 'test';
+</script>
