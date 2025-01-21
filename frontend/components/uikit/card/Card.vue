a8d175f979da78e93acb2593a2d98e011207833b
Efren Lim <elim@linuxfoundation.org>
2025-01-21 12:02:20 +0800

    added card component and tailwind configs
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/components/uikit/card/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-card">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Card"
+};
+</script>
+
+<style lang="scss" scoped src="./card.scss"></style>
+
