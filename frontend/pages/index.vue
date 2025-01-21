981ede9da42e4801649bae2c693d58995776364d
Gašper Grom <gasper.grom@gmail.com>
2025-01-21 17:40:41 +0100

    Font scheme

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="border text-primary-500 bg-white p-32">Sample Page</h1>
+    <h1 class="border font-secondary font-semibold text-primary-500 bg-white p-32">Sample Page</h1>
   </div>
 </template>
 
