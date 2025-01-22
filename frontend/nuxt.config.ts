2c6987a022471c675a8c75ea7f3eb2c16a200752
Efren Lim <elim@linuxfoundation.org>
2025-01-22 14:56:32 +0800

    added primevue and a sample button component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
-    devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
-    tailwindcss: {
-        exposeConfig: true,
-        viewer: {endpoint: "/_tailwind", exportViewer: true}
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@primevue/nuxt-module'],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: { endpoint: '/_tailwind', exportViewer: true },
+  },
+  css: ['~/assets/styles/main.scss'],
+  primevue: {
+    options: {
+      theme: 'none',
     },
-    css: ["~/assets/styles/main.scss"]
+  },
 });
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,10 +2,37 @@
   <div>
     <h1 class="border text-primary-500 bg-white p-32">Sample Page</h1>
     <h1>{{ sampleFunction() }}</h1>
+    <div class="flex flex-row gap-2">
+      <lfx-button>
+        Primary
+      </lfx-button>
+      <lfx-button type="secondary">
+        Secondary
+      </lfx-button>
+      <lfx-button type="success">
+        Success
+      </lfx-button>
+      <lfx-button type="info">
+        Info
+      </lfx-button>
+      <lfx-button type="warn">
+        Warn
+      </lfx-button>
+      <lfx-button type="help">
+        Help
+      </lfx-button>
+      <lfx-button type="danger">
+        Danger
+      </lfx-button>
+      <lfx-button type="contrast">
+        Contrast
+      </lfx-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import LfxButton from '~/components/uikit/lfx-button/LfxButton.vue';
 const test = "test";
 const sampleFunction = () => {
     return test;
