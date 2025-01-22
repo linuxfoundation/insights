3f3f833611467667c1c8baa458f3747d1e5d0272
Efren Lim <elim@linuxfoundation.org>
2025-01-22 14:56:32 +0800

    added primevue and a sample button component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/components/uikit/lfx-button/LfxButton.vue
@@ -0,0 +1,22 @@
+<!-- NOTE: This filename and export name must be LfxButton.vue otherwise it causes a circular error with PrimeVue -->
+<template>
+  <Button :severity="props.type">
+    <slot />
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { type ButtonType } from './types/ButtonType';
+
+const props = withDefaults(defineProps<{
+  type?: ButtonType,
+}>(), {
+  type: 'primary',
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxButton',
+};
+</script>
---
+++ frontend/components/uikit/lfx-button/types/ButtonType.ts
@@ -0,0 +1,3 @@
+export const buttonTypes = ['primary', 'secondary', 'success', 'info', 'warn', 'help', 'danger', 'contrast'] as const;
+
+export type ButtonType = (typeof buttonTypes)[number];
