0ea2f06a3c3c2dd007bcc5bda0e1db97e9ed570d
Efren Lim <elim@linuxfoundation.org>
2025-01-23 13:30:41 +0800

    added icon component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/components/uikit/icon/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <i :class="`${props.name} fa-${props.type}`" :style="sizeStyle" class="c-icon" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<{
+  name: string,
+  type?: string,
+  size?: number,
+}>(), {
+  type: 'light',
+  size: undefined,
+});
+
+const sizeStyle = computed(() => {
+  if (!props.size) {
+    return undefined;
+  }
+  return {
+    '--lf-icon-size': `${(props.size / 16)}rem`,
+  };
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxIcon',
+};
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,31 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    app: {
-        head: {
-            link: [
-                {rel: "preconnect", href: "https://fonts.googleapis.com"},
-                {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous"},
-                {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Roboto+Slab:wght@400;600&display=swap"}
-            ]
-        }
-    },
-    compatibilityDate: "2024-11-01",
-    devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/eslint", "@primevue/nuxt-module"],
-    tailwindcss: {
-        exposeConfig: true,
-        viewer: {endpoint: "/_tailwind", exportViewer: true}
-    },
-    css: ["~/assets/styles/main.scss"],
-    primevue: {
-        autoImport: false,
-        components: {
-            prefix: "pv",
-            include: ["Button", "DataTable"]
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Roboto+Slab:wght@400;600&display=swap',
         },
-        options: {
-            theme: "none" // This setting means we have to manually add styles to all the components
-        }
-    }
+      ],
+      script: [
+        // Using fontawesome like this instead of installing plugins
+        { src: 'https://kit.fontawesome.com/b5289aebdf.js' },
+      ],
+    },
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/eslint', '@primevue/nuxt-module'],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: { endpoint: '/_tailwind', exportViewer: true },
+  },
+  css: ['~/assets/styles/main.scss'],
+  primevue: {
+    autoImport: false,
+    components: {
+      prefix: 'pv',
+      include: ['Button', 'DataTable'],
+    },
+    options: {
+      theme: 'none', // This setting means we have to manually add styles to all the components
+    },
+  },
 });
