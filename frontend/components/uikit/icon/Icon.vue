91559a30d1404f3e8530060822005725b0286f42
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-01-23 15:24:13 +0800

    Improvement/ins 7 icons (#9)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>
    Co-authored-by: Gašper Grom <gasper.grom@gmail.com>

---
+++ frontend/components/uikit/icon/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <i :class="`fa-${props.name} fa-${props.type}`" :style="sizeStyle" class="c-icon" />
+</template>
+
+<script lang="ts" setup>
+import {computed} from "vue";
+
+const props = withDefaults(defineProps<{
+  name: string,
+  type?: "light" | "regular" | "solid" | "duotone" | "brands",
+  size?: number,
+}>(), {
+    type: "light",
+    size: 16
+});
+
+const sizeStyle = computed(() => ({
+    "--lf-icon-size": `${(props.size / 16)}rem`
+}));
+</script>
+
+<script lang="ts">
+export default {
+    name: "LfxIcon"
+};
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,14 @@ export default defineNuxtConfig({
             link: [
                 {rel: "preconnect", href: "https://fonts.googleapis.com"},
                 {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous"},
-                {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Roboto+Slab:wght@400;600&display=swap"}
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Roboto+Slab:wght@400;600&display=swap"
+                }
+            ],
+            script: [
+                // Using fontawesome like this instead of installing plugins
+                {src: "https://kit.fontawesome.com/b5289aebdf.js"}
             ]
         }
     },
