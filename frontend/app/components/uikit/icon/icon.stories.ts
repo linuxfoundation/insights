e0eecf78ef8ab64ac33aa2c2dc3f793141e71346
Gašper Grom <gasper.grom@gmail.com>
2025-01-29 14:09:56 +0100

    Icon docs

---
+++ frontend/app/components/uikit/icon/icon.stories.ts
@@ -0,0 +1,34 @@
+import LfxIcon from '@/components/uikit/icon/icon.vue';
+
+export default {
+  title: 'LinuxFoundation/Icon',
+  component: LfxIcon,
+  tags: ['autodocs'],
+  argTypes: {
+    // Props
+    name: {
+      description: 'Specifies icon name',
+      defaultValue: '',
+      control: 'text',
+    },
+    type: {
+      description: 'Specifies icon type',
+      defaultValue: 'light',
+      control: 'select',
+      options: ["light", "regular", "solid", "duotone"],
+    },
+    size: {
+      description: 'Specifies icon size in px',
+      defaultValue: '16',
+      control: 'number',
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    name: 'compass',
+    type: 'light',
+    size: 48,
+  },
+};
