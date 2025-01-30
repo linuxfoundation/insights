00e5960fffd916657d2857540cb3c74a373d0ea2
Efren Lim <elim@linuxfoundation.org>
2025-01-30 13:46:42 +0800

    added avatar group component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/uikit/avatar-group/avatar-group.stories.ts
@@ -0,0 +1,73 @@
+import LfxAvatar from '../avatar/avatar.vue';
+import LfxAvatarGroup from './avatar-group.vue';
+
+export default {
+  title: 'LinuxFoundation/AvatarGroup',
+  component: LfxAvatarGroup,
+  tags: ['autodocs'],
+  argTypes: {
+    // Slots
+    default: {
+      description: 'Text or html content of the button',
+      control: {
+        type: null
+      }
+    }
+  }
+};
+
+export const Default = {
+  args: {
+    default: 'Sample Name'
+  },
+  render: (args) => ({
+    components: { LfxAvatarGroup, LfxAvatar },
+    template: `<lfx-avatar-group>
+      <lfx-avatar name="${args.name}" size="normal" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="normal" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="normal" class="bg-brand-500" />
+    </lfx-avatar-group>`
+  })
+};
+
+export const Small = {
+  args: {
+    default: 'Sample Name'
+  },
+  render: (args) => ({
+    components: { LfxAvatarGroup, LfxAvatar },
+    template: `<lfx-avatar-group>
+      <lfx-avatar name="${args.name}" size="small" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="small" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="small" class="bg-brand-500" />
+    </lfx-avatar-group>`
+  })
+};
+
+export const Large = {
+  args: {
+    default: 'Sample Name'
+  },
+  render: (args) => ({
+    components: { LfxAvatarGroup, LfxAvatar },
+    template: `<lfx-avatar-group>
+      <lfx-avatar name="${args.name}" size="large" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="large" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="large" class="bg-brand-500" />
+    </lfx-avatar-group>`
+  })
+};
+
+export const XLarge = {
+  args: {
+    default: 'Sample Name'
+  },
+  render: (args) => ({
+    components: { LfxAvatarGroup, LfxAvatar },
+    template: `<lfx-avatar-group>
+      <lfx-avatar name="${args.name}" size="xlarge" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="xlarge" class="bg-brand-500" />
+      <lfx-avatar name="${args.name}" size="xlarge" class="bg-brand-500" />
+    </lfx-avatar-group>`
+  })
+};
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     autoImport: false,
     components: {
       prefix: 'pv',
-      include: ['Button', 'DataTable', 'Avatar']
+      include: ['Button', 'DataTable', 'Avatar', 'AvatarGroup']
     },
     options: {
       theme: 'none' // This setting means we have to manually add styles to all the components
