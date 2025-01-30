2aab29a0434389efc10e756801cc0eb5dec617a1
Gašper Grom <gasper.grom@gmail.com>
2025-01-30 09:09:47 +0100

    Split nuxt config

---
+++ frontend/setup/primevue.ts
@@ -0,0 +1,10 @@
+export default {
+    autoImport: false,
+    components: {
+        prefix: 'pv',
+        include: ['Button', 'DataTable']
+    },
+    options: {
+        theme: 'none' // This setting means we have to manually add styles to all the components
+    }
+}
