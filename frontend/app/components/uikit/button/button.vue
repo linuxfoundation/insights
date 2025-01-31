2f85c47deecf0ef9051bae1aa63663b802be26eb
Efren Lim <elim@linuxfoundation.org>
2025-01-31 15:40:40 +0800

    rename types file
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/uikit/button/button.vue
+++ frontend/app/components/uikit/button/button.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang="ts">
-import type { ButtonType, ButtonSize, IconPosition } from './types/ButtonType';
+import type { ButtonType, ButtonSize, IconPosition } from './types/button.types';
 
 const props = withDefaults(
   defineProps<{
