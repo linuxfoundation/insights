80190253828fba7d1db204360df40076a5332cd9
Efren Lim <elim@linuxfoundation.org>
2025-02-05 15:37:29 +0800

    added message component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/uikit/message/message.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    v-if="isVisible"
+    class="c-message"
+    :class="[`c-message--${props.messageStyle}`, `c-message--${props.size}`, `c-message--${props.type}`]">
+    <div class="c-message-header">
+      <i class="c-message-icon" :class="messageIcon" />
+      <span class="c-message-title">
+        {{ props.message }}
+      </span>
+      <i v-if="props.type !== 'transparent'" class="c-message-close fa-light fa-xmark" @click="closeMessage" />
+    </div>
+    <div v-if="props.type !== 'transparent'" class="c-message-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { MessageStyle, MessageSize, MessageType } from './types/message.types';
+
+const isVisible = ref(true);
+
+const props = withDefaults(
+  defineProps<{
+    message: string;
+    messageStyle?: MessageStyle;
+    size?: MessageSize;
+    type?: MessageType;
+  }>(),
+  {
+    messageStyle: 'default',
+    size: 'default',
+    type: 'box'
+  }
+);
+
+const messageIcon = computed(() => {
+  switch (props.messageStyle) {
+    case 'info':
+      return 'fa-solid fa-circle-info';
+    case 'positive':
+      return 'fa-solid fa-circle-check';
+    case 'warning':
+      return 'fa-solid fa-triangle-exclamation';
+    case 'negative':
+      return 'fa-solid fa-circle-exclamation';
+    default:
+      return 'fa-light fa-compass';
+  }
+});
+
+const closeMessage = () => {
+  isVisible.value = false;
+};
+</script>
+<script lang="ts">
+export default {
+  name: 'LfxMessage'
+};
+</script>
