89a33deaa7b868063e9fca2666b899d21090cf96
Gašper Grom <gasper.grom@gmail.com>
2025-02-05 06:37:09 +0100

    Tag component (#25)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

---
+++ frontend/app/components/uikit/tag/tag.stories.ts
@@ -0,0 +1,96 @@
+import LfxTag from './tag.vue';
+import {tagSizes, tagStyles, tagTypes} from "./types/tag.types";
+
+export default {
+  title: 'LinuxFoundation/Tag',
+  component: LfxTag,
+  tags: ['autodocs'],
+  argTypes: {
+    style: {
+      description: 'The style of the tag',
+      control: 'select',
+      options: tagStyles
+    },
+    size: {
+      description: 'The size of the tag',
+      control: 'select',
+      options: tagSizes
+    },
+    type: {
+      description: 'The type of the tag',
+      control: 'select',
+      options: tagTypes
+    },
+
+    // Slots
+    default: {
+      description: 'Text or html content of the tag',
+      control: {
+        type: null,
+      },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'default',
+    size: 'medium',
+    type: 'solid'
+  }
+};
+
+export const Info = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'info',
+    size: 'medium',
+    type: 'solid'
+  }
+};
+
+export const Positive = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'positive',
+    size: 'medium',
+    type: 'solid'
+  }
+};
+
+export const Warning = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'warning',
+    size: 'medium',
+    type: 'solid'
+  }
+};
+
+export const Negative = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'negative',
+    size: 'medium',
+    type: 'solid'
+  }
+};
+
+export const Small = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'info',
+    size: 'small',
+    type: 'solid'
+  }
+};
+
+export const Transparent = {
+  args: {
+    default: 'I\'m tag content',
+    style: 'info',
+    size: 'medium',
+    type: 'transparent'
+  }
+};
