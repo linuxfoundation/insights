f85b9aa36a03943a802e97414205bac0c4a3ea1a
Efren Lim <elim@linuxfoundation.org>
2025-02-06 16:08:57 +0800

    move plugin and add definition
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/plugins/toast.plugin.ts
+++ frontend/app/plugins/toast.plugin.ts
@@ -1,7 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import ToastService from 'primevue/toastservice';
+import { defineNuxtPlugin } from 'nuxt/app';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(ToastService);
