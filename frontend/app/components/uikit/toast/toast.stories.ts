5e7e0e93e4c2be9a26ba255f38035a0aa7b527e3
Efren Lim <elim@linuxfoundation.org>
2025-02-07 11:09:17 +0800

    fix toast story rendering
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/uikit/toast/toast.stories.ts
+++ frontend/app/components/uikit/toast/toast.stories.ts
@@ -1,63 +1,67 @@
-import Toast from "primevue/toast";
-import LfxButton from "../button/button.vue";
-import useToastService from "./toast.service";
-import {toastTypes} from "./types/toast.types";
+import LfxButton from '../button/button.vue';
+import LfxToast from './toast.vue';
+import useToastService from './toast.service';
+import type { ToastType } from './types/toast.types';
+import { ToastTypesEnum } from './types/toast.types';
 
 export default {
-  title: "LinuxFoundation/Toast",
-  tags: ["autodocs"],
+  title: 'LinuxFoundation/Toast',
+  tags: ['autodocs'],
   argTypes: {
     message: {
-      description: "Message of the toast",
-      control: "text",
+      description: 'Message of the toast',
+      control: 'text'
     },
-    toastType: {
-      description: 'The type of the toast',
-      control: 'select',
-      options: toastTypes,
-    },
+    // removing this as part of the argument because we cannot make this interactive
+    // Adding the <Toast /> component multiple times will result in duplicate toast messages
+    // toastType: {
+    //   description: 'The type of the toast',
+    //   control: 'select',
+    //   options: toastTypes
+    // },
     delay: {
-      description: "Delay of the toast",
-      control: "number",
-    },
-    icon: {
-      description: "Delay of the toast",
-      control: "string",
-    },
-  },
+      description: 'Delay of the toast',
+      control: 'number'
+    }
+  }
 };
 
 const render = (args) => ({
-  components: { LfxButton, Toast }, // Include Toast component
+  components: { LfxButton, LfxToast },
   setup() {
     const { showToast } = useToastService();
-    const displayToast = () => {
-      showToast(args.message, args.toastType, args.icon, args.delay);
+    const displayToast = (toastType: ToastType, icon?: string) => {
+      console.log('args', args);
+      showToast(args.message, toastType, icon, args.delay);
     };
-    return { args, displayToast };
+    const typesEnum = ToastTypesEnum;
+    const theme = 'dark';
+    return {
+      args,
+      displayToast,
+      typesEnum,
+      theme
+    };
   },
   template: `
     <div>
-      <Toast /> <!-- Ensure Toast is rendered -->
-      <lfx-button @click="displayToast">Show Toast</lfx-button>
+      <lfx-toast :theme="theme" /> <!-- Ensure Toast is rendered -->
+      <div class="flex flex-row gap-2">
+        <lfx-button label="Show Default" @click="displayToast(typesEnum.default)" />
+        <lfx-button label="Show Info" @click="displayToast(typesEnum.info)" />
+        <lfx-button label="Show Positive" @click="displayToast(typesEnum.positive)" />
+        <lfx-button label="Show Warning" @click="displayToast(typesEnum.warning)" />
+        <lfx-button label="Show Negative" @click="displayToast(typesEnum.negative)" />
+        <lfx-button label="Show Custom Icon" @click="displayToast(typesEnum.default, 'fa-solid fa-face-smile')" />
+      </div>
     </div>
-  `,
+  `
 });
 
 export const Default = {
   args: {
-    message: "This is a toast message",
-    toastType: 'secondary',
-    delay: 3000,
+    message: 'This is a toast message',
+    delay: 3000
   },
-  render,
-};
-
-export const Info = {
-  args: {
-    message: "This is a toast message",
-    toastType: 'info',
-    delay: 3000,
-  },
-  render,
+  render
 };
