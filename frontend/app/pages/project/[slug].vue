f6b3db14e06c6b76fdf37174cac5c450838b2062
Gašper Grom <gasper.grom@gmail.com>
2025-02-09 14:18:35 +0100

    Project routing (#32)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/pages/project/[slug].vue
+++ frontend/app/pages/project/[slug].vue
@@ -1,31 +1,5 @@
 <template>
-  <div class="py-8 bg-white border-b border-neutral-200 sticky top-header">
-    <div class="container">
-      <h1 class="text-heading-1 font-semibold pb-4">{{ slug }}</h1>
-      <div class="flex items-center gap-3">
-        <lfx-menu-button :to="{name: LfxRoutes.PROJECT}" exact>
-          <lfx-icon name="gauge-high" />
-          Overview
-        </lfx-menu-button>
-        <lfx-menu-button :to="{name: LfxRoutes.PROJECT_CONTRIBUTORS}">
-          <lfx-icon name="people-group" />
-          Contributors
-        </lfx-menu-button>
-        <lfx-menu-button :to="{name: LfxRoutes.PROJECT_POPULARITY}">
-          <lfx-icon name="fire" />
-          Popularity
-        </lfx-menu-button>
-        <lfx-menu-button :to="{name: LfxRoutes.PROJECT_DEVELOPMENT}">
-          <lfx-icon name="code" />
-          Development
-        </lfx-menu-button>
-        <lfx-menu-button :to="{name: LfxRoutes.PROJECT_SECURITY}">
-          <lfx-icon name="shield-check" />
-          Security & Best Practices
-        </lfx-menu-button>
-      </div>
-    </div>
-  </div>
+  <lfx-project-header :slug="slug" />
   <div>
     <nuxt-page />
   </div>
@@ -33,9 +7,7 @@
 
 <script setup lang="ts">
 import {useRoute} from "nuxt/app";
-import LfxMenuButton from "~/components/uikit/menu-button/menu-button.vue";
-import {LfxRoutes} from "~/components/shared/types/routes";
-import LfxIcon from "~/components/uikit/icon/icon.vue";
+import LfxProjectHeader from "~/components/modules/project/components/shared/header.vue";
 
 const route = useRoute();
 const {slug} = route.params;
