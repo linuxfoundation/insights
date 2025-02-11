9d6c50380225d8a6aee65f4936a15061887d0a6e
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-02-11 16:08:34 +0800

    Improvement/ins 43 select (#30)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/setup/primevue.ts
+++ frontend/setup/primevue.ts
@@ -2,7 +2,7 @@ export default {
   autoImport: false,
   components: {
     prefix: 'pv',
-    include: ['Button', 'DataTable', 'Avatar', 'AvatarGroup', 'SelectButton', 'Toast', 'Popover']
+    include: ['Button', 'DataTable', 'Avatar', 'AvatarGroup', 'SelectButton', 'Toast', 'Popover', 'Select']
   },
   options: {
     theme: 'none' // This setting means we have to manually add styles to all the components
