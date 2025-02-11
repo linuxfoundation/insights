396359978713e2f50c8fdff77b238d5d59ec53aa
Efren Lim <elim@linuxfoundation.org>
2025-02-11 16:58:25 +0800

    added loading status
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/uikit/spinner/spinner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center h-full w-full">
+    <i class="fa-light fa-spinner-third animate-spin" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'LfxSpinner'
+};
+</script>
