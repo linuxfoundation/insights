dfac80d396046bc44dd5d344b2fc01a3010ae192
Gašper Grom <gasper.grom@gmail.com>
2025-02-12 18:14:15 +0100

    Search functionalityg
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

---
+++ frontend/app/components/shared/types/search.ts
@@ -0,0 +1,24 @@
+export interface SearchCollection {
+    name: string;
+    slug: string;
+}
+
+export interface SearchProject {
+    name: string;
+    slug: string;
+    logo: string | null;
+}
+
+export interface SearchRepository {
+    name: string;
+    slug: string;
+    projecName: string;
+    projectSlug: string;
+}
+
+
+export interface SearchResults {
+    projects: SearchProject[];
+    repositories: SearchRepository[];
+    collections: SearchCollection[];
+}
