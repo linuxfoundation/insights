05b58e850f3ac05a4b05fc3ad126427988001d4a
Efren Lim <elim@linuxfoundation.org>
2025-02-13 17:06:02 +0800

    fix time periods list
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/components/contributors/fragments/dependency-display.vue
+++ frontend/app/components/modules/project/components/contributors/fragments/dependency-display.vue
@@ -5,14 +5,16 @@
         <slot />
       </div>
       <div class="flex flex-col items-start">
-        <div class="text-sm font-semibold">{{ props.topContributors.count }} {{ label }}</div>
+        <div class="text-sm font-semibold">{{ props.topContributors.count }} {{ props.label }}</div>
         <div :class="`text-body-1 text-${dependencyColor}-500`">
           {{ props.topContributors.percentage }}% of all contributions
         </div>
       </div>
     </div>
     <div class="flex flex-col items-end">
-      <div class="text-sm font-semibold">Other {{ props.otherContributors.count }} {{ label }}</div>
+      <div class="text-sm font-semibold">
+        Other {{ props.otherContributors.count }} {{ props.label }}
+      </div>
       <div class="text-body-1 text-neutral-500">
         {{ props.otherContributors.percentage }}% of all contributions
       </div>
@@ -28,7 +30,6 @@ import type { Dependency } from '~/components/shared/types/contributors.types';
 import type { ProgressBarType } from '~/components/uikit/progress-bar/types/progress-bar.types';
 </script>
 <script lang="ts">
-
 const props = defineProps<{
   topContributors: Dependency;
   otherContributors: Dependency;
--- frontend/app/components/shared/types/time-periods.ts
+++ frontend/app/components/shared/types/time-periods.ts
@@ -1,5 +1,11 @@
 export const timePeriodsOptions = [
   { label: 'Last 90 days', value: '90d' },
-  { label: 'Last 1 year', value: '1y' },
-  { label: 'Last 3 years', value: '3y' }
+  { label: 'Last 180 days', value: '180d' },
+  { label: 'Last 365 days', value: '365d' },
+  { label: 'Previous quarter', value: 'pq' },
+  { label: 'Previous year', value: 'py' },
+  { label: 'Previous 5 years', value: 'p5y' },
+  { label: 'Previous 10 years', value: 'p10y' },
+  { label: 'All time', value: 'all' },
+  { label: 'Custom', value: 'custom' }
 ];
