8690bef3b12bd111f9593582b405a8216b8daa9b
Efren Lim <elim@linuxfoundation.org>
2025-02-13 09:57:08 +0800

    fix dropdown issue
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/collection/views/collection-list.vue
+++ frontend/app/components/modules/collection/views/collection-list.vue
@@ -15,48 +15,47 @@
   <section class="sticky top-14 lg:top-17 bg-white">
     <div class="container py-5">
       <div class="flex justify-between items-center">
-        <div>
-          Filters
-        </div>
+        <div>Filters</div>
         <lfx-dropdown
-          v-model="sort" :options="sortOptions"
+          v-model="sort"
+          :options="sortOptions"
           icon="fa-arrow-down-wide-short fa-light"
           type="transparent"
-        />
+          dropdown-position="right" />
       </div>
     </div>
   </section>
   <section>
     <div class="container">
       <div class="flex flex-col gap-8 pt-10 pb-64">
-        <lfx-collection-list-item  v-for="i of 12" :key="i" />
+        <lfx-collection-list-item v-for="i of 12" :key="i" />
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import LfxIcon from "~/components/uikit/icon/icon.vue";
-import LfxTag from "~/components/uikit/tag/tag.vue";
-import LfxDropdown from "~/components/uikit/dropdown/dropdown.vue";
-import LfxCollectionListItem from "~/components/modules/collection/components/collection-list-item.vue";
+import LfxIcon from '~/components/uikit/icon/icon.vue';
+import LfxTag from '~/components/uikit/tag/tag.vue';
+import LfxDropdown from '~/components/uikit/dropdown/dropdown.vue';
+import LfxCollectionListItem from '~/components/modules/collection/components/collection-list-item.vue';
 
 const sortOptions = [
   {
     label: 'Most contributors',
-    value: 'contributors_DESC',
+    value: 'contributors_DESC'
   },
   {
     label: 'Alphabeticly',
-    value: 'name_DESC',
-  },
+    value: 'name_DESC'
+  }
 ];
 
-const sort = ref('contributors_DESC')
+const sort = ref('contributors_DESC');
 </script>
 
 <script lang="ts">
 export default {
   name: 'LfxCollectionListView'
-}
+};
 </script>
