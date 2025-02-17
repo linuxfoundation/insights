5c50050a2b0cfb05a7cf94986ee57f7e17da2ecd
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-02-17 14:04:55 +0800

    added retention widget (#39)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/server/api/contributors/retention.get.ts
@@ -0,0 +1,34 @@
+import { contributorRetention, organizationRetention } from '~~/server/mocks/retention.mock';
+
+/**
+ * Frontend expects the data to be in the following format:
+ * {
+ *   dateFrom: string; // ISO 8601 date string - start of the bucket. Based on the interval
+ *   dateTo: string; // ISO 8601 date string - end of the bucket. Based on the interval
+ *   percentage: number; // percentage retention
+ * }[]
+ */
+/**
+ * Query params:
+ * - type: 'contributors' | 'organizations'
+ * - project: string // slug
+ * - repository: string // In the old Insights this is the fully qualified URL of the repo
+ * - time-period: string // see below
+ */
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  let data;
+
+  if (query.type === 'contributors') {
+    data = contributorRetention;
+  } else if (query.type === 'organizations') {
+    data = organizationRetention;
+  } else {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Invalid type'
+    });
+  }
+
+  return data;
+});
