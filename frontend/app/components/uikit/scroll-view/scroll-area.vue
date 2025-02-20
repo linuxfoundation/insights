9bf5cfb3a2d49ac357411450820d078ce48093ca
Efren Lim <elim@linuxfoundation.org>
2025-02-20 15:11:05 +0800

    partial work on scrolling feature
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/uikit/scroll-view/scroll-area.vue
@@ -0,0 +1,41 @@
+<template>
+  <div ref="scrollAreaRef" class="scroll-area">
+    <slot :observer="observer" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const scrollAreaRef = ref<HTMLElement | null>(null);
+const observer = ref<IntersectionObserver | null>(null);
+const scrolledToView = new Event('scrolledToView');
+
+const handleIntersectCallback = (entries: IntersectionObserverEntry[]) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio >= 0.5) {
+      entry.target.dispatchEvent(scrolledToView);
+    }
+  });
+};
+
+onMounted(() => {
+  const options = {
+    root: null,
+    rootMargin: '100px 0px 0px 0px',
+    threshold: 0.5
+  };
+
+  observer.value = new IntersectionObserver(handleIntersectCallback, options);
+});
+
+onUnmounted(() => {
+  observer.value?.disconnect();
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxScrollArea'
+};
+</script>
---
+++ frontend/app/components/uikit/side-nav/side-nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="lfx-side-nav sticky top-[220px] flex flex-col gap-5">
+    <li v-for="item in props.list" :key="item.label">
+      <a
+        :href="`#${item.key}`"
+        :class="{ active: activeItem === item.key }"
+        @click="onClick(item.key, $event)"
+      >{{ item.label }}</a
+      >
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import type { SideNavItem } from './types/side-nav.types';
+
+const props = defineProps<{
+  list: SideNavItem[];
+  modelValue: string;
+}>();
+const router = useRouter();
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void }>();
+
+const activeItem = computed<string>({
+  get() {
+    return props.modelValue;
+  },
+  set(value: string) {
+    emit('update:modelValue', value);
+  }
+});
+
+const onClick = (value: string, e: Event) => {
+  activeItem.value = value;
+  e.preventDefault();
+  e.stopPropagation();
+
+  router.replace({
+    hash: `#${value}`
+  });
+};
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxSideNav'
+};
+</script>
