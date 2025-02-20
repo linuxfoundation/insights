dc9ec62f6187c50adb783071424e629d59bfffbd
Gašper Grom <gasper.grom@gmail.com>
2025-02-20 12:52:24 +0100

    add scroll percentage
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/shared/utils/scroll.ts
+++ frontend/app/components/shared/utils/scroll.ts
@@ -2,10 +2,17 @@ import {onMounted, onUnmounted, ref} from "vue";
 
 const useScroll = () => {
     const scrollTop = ref(0);
+    const scrollPercentage = ref(0);
     let body = document?.querySelector('body');
 
     const updateScrollTop = () => {
         scrollTop.value = body?.scrollTop || 0;
+
+        const scrollHeight = body?.scrollHeight || 0;
+        const clientHeight = body?.clientHeight || 0;
+        scrollPercentage.value = scrollHeight > clientHeight
+            ? (scrollTop.value / (scrollHeight - clientHeight)) * 100
+            : 0;
     };
 
     onMounted(() => {
@@ -20,6 +27,7 @@ const useScroll = () => {
 
     return {
         scrollTop,
+        scrollPercentage
     }
 }
 
