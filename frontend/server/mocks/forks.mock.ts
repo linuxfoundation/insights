820529fcb4e9557171f8030f4aa06fe329e1df2d
Efren Lim <elim@linuxfoundation.org>
2025-02-21 16:32:12 +0800

    added forks widget
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/server/mocks/forks.mock.ts
@@ -0,0 +1,185 @@
+export const cumulative = {
+  summary: {
+    current: 2000,
+    previous: 1800,
+    percentageChange: 11.1,
+    changeValue: 200,
+    periodFrom: '2024-11-11T00:00:00Z',
+    periodTo: '2025-02-12T00:00:00Z'
+  },
+  data: [
+    {
+      dateFrom: '2024-11-11T00:00:00Z',
+      dateTo: '2024-11-18T00:00:00Z',
+      forks: 1200
+    },
+    {
+      dateFrom: '2024-11-18T00:00:00Z',
+      dateTo: '2024-11-25T00:00:00Z',
+      forks: 1400
+    },
+    {
+      dateFrom: '2024-11-25T00:00:00Z',
+      dateTo: '2024-12-02T00:00:00Z',
+      forks: 1600
+    },
+    {
+      dateFrom: '2024-12-02T00:00:00Z',
+      dateTo: '2024-12-09T00:00:00Z',
+      forks: 1800
+    },
+    {
+      dateFrom: '2024-12-09T00:00:00Z',
+      dateTo: '2024-12-16T00:00:00Z',
+      forks: 2000
+    },
+    {
+      dateFrom: '2024-12-16T00:00:00Z',
+      dateTo: '2024-12-23T00:00:00Z',
+      forks: 2200
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      forks: 2400
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      forks: 2600
+    },
+    {
+      dateFrom: '2025-01-06T00:00:00Z',
+      dateTo: '2025-01-13T00:00:00Z',
+      forks: 2800
+    },
+    {
+      dateFrom: '2025-01-13T00:00:00Z',
+      dateTo: '2025-01-20T00:00:00Z',
+      forks: 3000
+    },
+    {
+      dateFrom: '2025-01-20T00:00:00Z',
+      dateTo: '2025-01-27T00:00:00Z',
+      forks: 3200
+    },
+    {
+      dateFrom: '2025-01-27T00:00:00Z',
+      dateTo: '2025-02-03T00:00:00Z',
+      forks: 3400
+    },
+    {
+      dateFrom: '2025-02-03T00:00:00Z',
+      dateTo: '2025-02-10T00:00:00Z',
+      forks: 3600
+    },
+    {
+      dateFrom: '2025-02-10T00:00:00Z',
+      dateTo: '2025-02-17T00:00:00Z',
+      forks: 3800
+    },
+    {
+      dateFrom: '2025-02-17T00:00:00Z',
+      dateTo: '2025-02-24T00:00:00Z',
+      forks: 4000
+    },
+    {
+      dateFrom: '2025-02-24T00:00:00Z',
+      dateTo: '2025-03-03T00:00:00Z',
+      forks: 4200
+    }
+  ]
+};
+
+export const newStars = {
+  summary: {
+    current: 1200,
+    previous: 1000,
+    percentageChange: 20,
+    changeValue: 200,
+    periodFrom: '2024-11-11T00:00:00Z',
+    periodTo: '2025-02-12T00:00:00Z'
+  },
+  data: [
+    {
+      dateFrom: '2024-11-11T00:00:00Z',
+      dateTo: '2024-11-18T00:00:00Z',
+      forks: 120
+    },
+    {
+      dateFrom: '2024-11-18T00:00:00Z',
+      dateTo: '2024-11-25T00:00:00Z',
+      forks: 400
+    },
+    {
+      dateFrom: '2024-11-25T00:00:00Z',
+      dateTo: '2024-12-02T00:00:00Z',
+      forks: 600
+    },
+    {
+      dateFrom: '2024-12-02T00:00:00Z',
+      dateTo: '2024-12-09T00:00:00Z',
+      forks: 180
+    },
+    {
+      dateFrom: '2024-12-09T00:00:00Z',
+      dateTo: '2024-12-16T00:00:00Z',
+      forks: 200
+    },
+    {
+      dateFrom: '2024-12-16T00:00:00Z',
+      dateTo: '2024-12-23T00:00:00Z',
+      forks: 220
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      forks: 240
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      forks: 600
+    },
+    {
+      dateFrom: '2025-01-06T00:00:00Z',
+      dateTo: '2025-01-13T00:00:00Z',
+      forks: 280
+    },
+    {
+      dateFrom: '2025-01-13T00:00:00Z',
+      dateTo: '2025-01-20T00:00:00Z',
+      forks: 300
+    },
+    {
+      dateFrom: '2025-01-20T00:00:00Z',
+      dateTo: '2025-01-27T00:00:00Z',
+      forks: 320
+    },
+    {
+      dateFrom: '2025-01-27T00:00:00Z',
+      dateTo: '2025-02-03T00:00:00Z',
+      forks: 400
+    },
+    {
+      dateFrom: '2025-02-03T00:00:00Z',
+      dateTo: '2025-02-10T00:00:00Z',
+      forks: 600
+    },
+    {
+      dateFrom: '2025-02-10T00:00:00Z',
+      dateTo: '2025-02-17T00:00:00Z',
+      forks: 380
+    },
+    {
+      dateFrom: '2025-02-17T00:00:00Z',
+      dateTo: '2025-02-24T00:00:00Z',
+      forks: 400
+    },
+    {
+      dateFrom: '2025-02-24T00:00:00Z',
+      dateTo: '2025-03-03T00:00:00Z',
+      forks: 200
+    }
+  ]
+};
