70045a05b90106501287033e81bc4c65321da349
Efren Lim <elim@linuxfoundation.org>
2025-02-25 17:25:58 +0800

    hide other popularity widgets
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/views/popularity.vue
+++ frontend/app/components/modules/project/views/popularity.vue
@@ -7,15 +7,15 @@
       <div class="w-1/2 flex flex-col gap-12">
         <lfx-project-stars />
         <lfx-project-forks />
-        <lfx-project-social-mentions />
+        <!-- <lfx-project-social-mentions />
         <lfx-project-github-mentions />
         <lfx-project-press-mentions />
         <lfx-project-search-queries />
-        <lfx-project-package-downloads />
+        <lfx-project-package-downloads /> -->
       </div>
 
       <div class="w-1/4 pl-10">
-        <p>Feedback</p>
+
       </div>
     </div>
   </div>
@@ -25,11 +25,11 @@
 
 import LfxProjectStars from "~/components/modules/project/components/popularity/stars.vue";
 import LfxProjectForks from "~/components/modules/project/components/popularity/forks.vue";
-import LfxProjectSocialMentions from "~/components/modules/project/components/popularity/social-mentions.vue";
-import LfxProjectGithubMentions from "~/components/modules/project/components/popularity/github-mentions.vue";
-import LfxProjectPressMentions from "~/components/modules/project/components/popularity/press-mentions.vue";
-import LfxProjectSearchQueries from "~/components/modules/project/components/popularity/search-queries.vue";
-import LfxProjectPackageDownloads from "~/components/modules/project/components/popularity/package-downloads.vue";
+// import LfxProjectSocialMentions from "~/components/modules/project/components/popularity/social-mentions.vue";
+// import LfxProjectGithubMentions from "~/components/modules/project/components/popularity/github-mentions.vue";
+// import LfxProjectPressMentions from "~/components/modules/project/components/popularity/press-mentions.vue";
+// import LfxProjectSearchQueries from "~/components/modules/project/components/popularity/search-queries.vue";
+// import LfxProjectPackageDownloads from "~/components/modules/project/components/popularity/package-downloads.vue";
 </script>
 
 <script lang="ts">
