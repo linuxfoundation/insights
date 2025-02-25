a39ccebbd647a9595185f4771ec2946b840037d7
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-02-25 09:53:35 +0800

    Social, github and press mentions widgets (#47)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/server/api/projects/contributors/social-mentions.get.ts
@@ -0,0 +1,53 @@
+import { allTime, newMentions } from '~~/server/mocks/social-mentions.mock';
+
+/**
+ * Frontend expects the data to be in the following format:
+ * {
+ *   summary: {
+ *     current: number; // current value
+ *     previous: number; // previous value
+ *     percentageChange: number; // percentage change (return as actual percentage ex: 2.3 percent)
+ *     changeValue: number; // change value
+ *     periodFrom: string; // period from
+ *     periodTo: string; // period to
+ *   },
+ *   data: {
+ *     dateFrom: string; // ISO 8601 date string - start of the bucket. Based on the interval
+ *     dateTo: string; // ISO 8601 date string - end of the bucket. Based on the interval
+ *     twitter: number; // count of twitter mentions
+ *     reddit: number; // count of reddit mentions
+ *     hackerNews: number; // count of hackerNews mentions
+ *     stackOverflow: number; // count of stackOverflow mentions
+ *   }[];
+ * }
+ */
+/**
+ * Query params:
+ * - interval: 'all-time' | 'new-mentions'
+ * - project: string
+ * - repository: string
+ * - time-period: string // This is isn't defined yet, but we'll add '90d', '1y', '5y' for now
+ */
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  let data;
+
+  if (query.interval === 'all-time') {
+    data = allTime;
+  } else {
+    data = newMentions;
+  }
+
+  // doing fake changes to data if query.repository is not empty
+  if (query.repository) {
+    data.data = data.data.map((item) => ({
+      ...item,
+      twitter: item.twitter - 300,
+      reddit: item.reddit - 100,
+      hackerNews: item.hackerNews - 200,
+      stackOverflow: item.stackOverflow - 100
+    }));
+  }
+
+  return data;
+});
---
+++ frontend/server/mocks/github-mentions.mock.ts
@@ -0,0 +1,185 @@
+export const newMentions = {
+  summary: {
+    current: 400,
+    previous: 380,
+    percentageChange: 3.1,
+    changeValue: 20,
+    periodFrom: '2024-11-11T00:00:00Z',
+    periodTo: '2025-02-12T00:00:00Z'
+  },
+  data: [
+    {
+      dateFrom: '2024-11-11T00:00:00Z',
+      dateTo: '2024-11-18T00:00:00Z',
+      mentions: 8934
+    },
+    {
+      dateFrom: '2024-11-18T00:00:00Z',
+      dateTo: '2024-11-25T00:00:00Z',
+      mentions: 6012
+    },
+    {
+      dateFrom: '2024-11-25T00:00:00Z',
+      dateTo: '2024-12-02T00:00:00Z',
+      mentions: 8876
+    },
+    {
+      dateFrom: '2024-12-02T00:00:00Z',
+      dateTo: '2024-12-09T00:00:00Z',
+      mentions: 5145
+    },
+    {
+      dateFrom: '2024-12-09T00:00:00Z',
+      dateTo: '2024-12-16T00:00:00Z',
+      mentions: 8998
+    },
+    {
+      dateFrom: '2024-12-16T00:00:00Z',
+      dateTo: '2024-12-23T00:00:00Z',
+      mentions: 5234
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      mentions: 9087
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      mentions: 7321
+    },
+    {
+      dateFrom: '2025-01-06T00:00:00Z',
+      dateTo: '2025-01-13T00:00:00Z',
+      mentions: 5156
+    },
+    {
+      dateFrom: '2025-01-13T00:00:00Z',
+      dateTo: '2025-01-20T00:00:00Z',
+      mentions: 9432
+    },
+    {
+      dateFrom: '2025-01-20T00:00:00Z',
+      dateTo: '2025-01-27T00:00:00Z',
+      mentions: 9267
+    },
+    {
+      dateFrom: '2025-01-27T00:00:00Z',
+      dateTo: '2025-02-03T00:00:00Z',
+      mentions: 4543
+    },
+    {
+      dateFrom: '2025-02-03T00:00:00Z',
+      dateTo: '2025-02-10T00:00:00Z',
+      mentions: 3378
+    },
+    {
+      dateFrom: '2025-02-10T00:00:00Z',
+      dateTo: '2025-02-17T00:00:00Z',
+      mentions: 6654
+    },
+    {
+      dateFrom: '2025-02-17T00:00:00Z',
+      dateTo: '2025-02-24T00:00:00Z',
+      mentions: 4489
+    },
+    {
+      dateFrom: '2025-02-24T00:00:00Z',
+      dateTo: '2025-03-03T00:00:00Z',
+      mentions: 9765
+    }
+  ]
+};
+
+export const cumulative = {
+  summary: {
+    current: 4000,
+    previous: 3880,
+    percentageChange: 3.1,
+    changeValue: 120,
+    periodFrom: '2024-11-11T00:00:00Z',
+    periodTo: '2025-02-12T00:00:00Z'
+  },
+  data: [
+    {
+      dateFrom: '2024-11-11T00:00:00Z',
+      dateTo: '2024-11-18T00:00:00Z',
+      mentions: 234
+    },
+    {
+      dateFrom: '2024-11-18T00:00:00Z',
+      dateTo: '2024-11-25T00:00:00Z',
+      mentions: 312
+    },
+    {
+      dateFrom: '2024-11-25T00:00:00Z',
+      dateTo: '2024-12-02T00:00:00Z',
+      mentions: 376
+    },
+    {
+      dateFrom: '2024-12-02T00:00:00Z',
+      dateTo: '2024-12-09T00:00:00Z',
+      mentions: 445
+    },
+    {
+      dateFrom: '2024-12-09T00:00:00Z',
+      dateTo: '2024-12-16T00:00:00Z',
+      mentions: 498
+    },
+    {
+      dateFrom: '2024-12-16T00:00:00Z',
+      dateTo: '2024-12-23T00:00:00Z',
+      mentions: 534
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      mentions: 587
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      mentions: 621
+    },
+    {
+      dateFrom: '2025-01-06T00:00:00Z',
+      dateTo: '2025-01-13T00:00:00Z',
+      mentions: 656
+    },
+    {
+      dateFrom: '2025-01-13T00:00:00Z',
+      dateTo: '2025-01-20T00:00:00Z',
+      mentions: 732
+    },
+    {
+      dateFrom: '2025-01-20T00:00:00Z',
+      dateTo: '2025-01-27T00:00:00Z',
+      mentions: 767
+    },
+    {
+      dateFrom: '2025-01-27T00:00:00Z',
+      dateTo: '2025-02-03T00:00:00Z',
+      mentions: 843
+    },
+    {
+      dateFrom: '2025-02-03T00:00:00Z',
+      dateTo: '2025-02-10T00:00:00Z',
+      mentions: 878
+    },
+    {
+      dateFrom: '2025-02-10T00:00:00Z',
+      dateTo: '2025-02-17T00:00:00Z',
+      mentions: 954
+    },
+    {
+      dateFrom: '2025-02-17T00:00:00Z',
+      dateTo: '2025-02-24T00:00:00Z',
+      mentions: 989
+    },
+    {
+      dateFrom: '2025-02-24T00:00:00Z',
+      dateTo: '2025-03-03T00:00:00Z',
+      mentions: 1065
+    }
+  ]
+};
