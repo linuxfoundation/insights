bbf4bedc54fbddf94a77c8a358be3edf4ca2418b
Gašper Grom <gasper.grom@gmail.com>
2025-02-27 09:18:27 +0100

    Collection list & filters (#48)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

---
+++ frontend/app/components/modules/collection/components/list/filters/collection-filter-stack.vue
@@ -0,0 +1,51 @@
+<template>
+  <lfx-dropdown
+    v-model="value"
+    :options="options"
+    icon="fa-layer-group fa-light"
+    type="filled"
+    dropdown-position="left"
+    prefix="Stack"
+    placeholder="All"
+    :split-lines="[1]"
+  />
+</template>
+
+<script lang="ts" setup>
+import {computed} from "vue";
+import {useFetch} from "nuxt/app";
+import LfxDropdown from "~/components/uikit/dropdown/dropdown.vue";
+import type {DropdownGroupOptions} from "~/components/uikit/dropdown/types/dropdown.types";
+
+const props = defineProps<{
+  modelValue: string;
+}>()
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void }>();
+
+const value = computed({
+  get: () => props.modelValue,
+  set: (value: string) => emit('update:modelValue', value)
+});
+
+const options = computed<DropdownGroupOptions>(() => [
+  {
+    label: '',
+    items: [
+      { label: 'All Stacks', value: '' },
+    ]
+  },
+    ...(data.value || []),
+])
+
+const { data } = useFetch<DropdownGroupOptions>(
+    () => `/api/collections/stack`
+);
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxCollectionFilterStack'
+};
+</script>
--- frontend/app/components/modules/collection/views/collection-list.vue
+++ frontend/app/components/modules/collection/views/collection-list.vue
@@ -14,8 +14,11 @@
   </section>
   <section class="sticky top-14 lg:top-17 bg-white">
     <div class="container py-5">
-      <div class="flex justify-between items-center">
-        <div>Filters</div>
+      <div class="flex justify-between items-center gap-4 flex-wrap">
+        <div class="flex items-center gap-4">
+          <lfx-collection-filter-stack v-model="stack" />
+          <lfx-collection-filter-industry v-model="industry" />
+        </div>
         <lfx-dropdown
           v-model="sort"
           :options="sortOptions"
@@ -27,7 +30,10 @@
   </section>
   <section>
     <div class="container">
-      <div class="flex flex-col gap-8 pt-10 pb-16">
+      <div v-if="status === 'pending'" class="flex items-center justify-between h-32 py-1">
+        <lfx-spinner :size="40" class=" text-neutral-300" />
+      </div>
+      <div v-else class="flex flex-col gap-8 pt-10 pb-16">
         <lfx-collection-list-item
           v-for="collection of (data?.data || [])"
           :key="collection.slug"
@@ -40,31 +46,60 @@
 
 <script setup lang="ts">
 import {useFetch} from "nuxt/app";
+import {watch} from "vue";
 import LfxIcon from '~/components/uikit/icon/icon.vue';
 import LfxTag from '~/components/uikit/tag/tag.vue';
 import LfxDropdown from '~/components/uikit/dropdown/dropdown.vue';
 import LfxCollectionListItem from '~/components/modules/collection/components/collection-list-item.vue';
 import type {Pagination} from "~/components/shared/types/pagination";
 import type {Collection} from "~/components/modules/collection/types/Collection";
+import LfxCollectionFilterStack
+  from "~/components/modules/collection/components/list/filters/collection-filter-stack.vue";
+import LfxCollectionFilterIndustry
+  from "~/components/modules/collection/components/list/filters/collection-filter-industry.vue";
+import LfxSpinner from "~/components/uikit/spinner/spinner.vue";
+import {ToastTypesEnum} from "~/components/uikit/toast/types/toast.types";
+import useToastService from "~/components/uikit/toast/toast.service";
 
-const sortOptions = [
-  {
-    label: 'Most contributors',
-    value: 'contributorsCount_DESC'
-  },
-  {
-    label: 'Alphabeticly',
-    value: 'name_DESC'
-  }
-];
+const { showToast } = useToastService();
 
 const page = ref(1);
 const pageSize = ref(50);
 const sort = ref('name_DESC');
 
-const { data } = useFetch<Pagination<Collection>>(
-    () => `/api/collections?sort=${sort.value}&page=${page.value}&pageSize=${pageSize.value}`,
+const stack = ref('');
+const industry = ref('');
+
+const { data, status, error } = useFetch<Pagination<Collection>>(
+    () => `/api/collections?sort=${sort.value}&page=${page.value}&pageSize=${pageSize.value}
+    &stack=${stack.value}&industry=${industry.value}`,
 );
+
+watch(error, (err) => {
+  if (err) {
+    showToast(
+        `There was an error fetching collections`,
+        ToastTypesEnum.negative,
+        undefined,
+        5000
+    );
+  }
+});
+
+const sortOptions = [
+  {
+    label: 'Alphabetically',
+    value: 'name_DESC'
+  },
+  {
+    label: 'Most projects',
+    value: 'projectCount_DESC'
+  },
+  {
+    label: 'Most valuable',
+    value: 'softwareValueCount_DESC'
+  },
+];
 </script>
 
 <script lang="ts">
