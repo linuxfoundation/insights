8c9254263212f4049aa48be31c2cbb753213e4a0
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-02-27 15:35:50 +0800

    Improvement/contribution working hours (#54)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/samples/scatter-chart.sample.vue
+++ frontend/app/components/samples/scatter-chart.sample.vue
@@ -1,44 +1,45 @@
 <template>
   <div style="width: 100%; height: 500px">
-    <lfx-chart :config="getScatterChartConfig(props.chartData, chartSeries, categoryData)" />
+    <lfx-chart :config="getScatterChartConfig(props.chartData, chartSeries)" />
   </div>
 </template>
 
 <script setup lang="ts">
 import LfxChart from '~/components/uikit/chart/Chart.vue';
-import type { ChartData, ChartSeries, CategoryData } from '~/components/uikit/chart/types/ChartTypes';
+import type { ChartData, ChartSeries } from '~/components/uikit/chart/types/ChartTypes';
 import { getScatterChartConfig } from '~/components/uikit/chart/configs/scatter.chart';
-import {lfxColors} from "~/config/styles/colors";
+import { lfxColors } from "~/config/styles/colors";
 
 const props = defineProps<{
   chartData: ChartData[];
 }>();
 
-const categoryData: CategoryData = {
-  xAxis: [
-    { key: 'Mon', value: 1 },
-    { key: 'Tue', value: 2 },
-    { key: 'Wed', value: 3 },
-    { key: 'Thu', value: 4 },
-    { key: 'Fri', value: 5 },
-    { key: 'Sat', value: 6 },
-    { key: 'Sun', value: 0 }
-  ],
-  yAxis: [
-    { key: '0:00', value: 0 },
-    { key: '2:00', value: 2 },
-    { key: '4:00', value: 4 },
-    { key: '6:00', value: 6 },
-    { key: '8:00', value: 8 },
-    { key: '10:00', value: 10 },
-    { key: '12:00', value: 12 },
-    { key: '14:00', value: 14 },
-    { key: '16:00', value: 16 },
-    { key: '18:00', value: 18 },
-    { key: '20:00', value: 20 },
-    { key: '22:00', value: 22 }
-  ]
-};
+// const categoryData: CategoryData = {
+//   xAxis: [
+//     { key: 'Mon', value: 1 },
+//     { key: 'Tue', value: 2 },
+//     { key: 'Wed', value: 3 },
+//     { key: 'Thu', value: 4 },
+//     { key: 'Fri', value: 5 },
+//     { key: 'Sat', value: 6 },
+//     { key: 'Sun', value: 0 }
+//   ],
+//   yAxis: [
+//     { key: '8:00', value: 8 },
+//     { key: '10:00', value: 10 },
+//     { key: '12:00', value: 12 },
+//     { key: '14:00', value: 14 },
+//     { key: '16:00', value: 16 },
+//     { key: '18:00', value: 18 },
+//     { key: '20:00', value: 20 },
+//     { key: '22:00', value: 22 },
+//     { key: '0:00+1', value: 0 },
+//     { key: '2:00+1', value: 2 },
+//     { key: '4:00+1', value: 4 },
+//     { key: '6:00+1', value: 6 },
+
+//   ]
+// };
 
 const chartSeries = ref<ChartSeries[]>([
   {
