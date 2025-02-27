b4073c1f1eeb79e17c4a57a68f6964f0f6711cbb
Gašper Grom <gasper.grom@gmail.com>
2025-02-27 09:40:19 +0100

    Fix issue with project header
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/pages/project/[slug].vue
+++ frontend/app/pages/project/[slug].vue
@@ -6,9 +6,5 @@
 </template>
 
 <script setup lang="ts">
-import {useRoute} from "nuxt/app";
 import LfxProjectHeader from "~/components/modules/project/components/shared/header.vue";
-
-const route = useRoute();
-const {slug} = route.params;
 </script>
