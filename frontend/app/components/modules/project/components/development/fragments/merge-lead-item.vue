1dea16d2477b9b190b48058fb0c3d287397584aa
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-02-28 15:39:36 +0800

    Improvement/merge lead time (#55)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/modules/project/components/development/fragments/merge-lead-item.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-row gap-4 items-center justify-between">
+    <div class="h-8 w-8 bg-neutral-100 rounded-full flex items-center justify-center relative">
+      <span class="absolute -top-6 left-[50%] h-6 border-l border-neutral-300 border-dashed -ml-[1px]" />
+      <lfx-icon :name="icon" :size="16" />
+      <span
+        v-if="!isLast"
+        class="absolute -bottom-5 left-[50%] h-5 border-l border-neutral-300 border-dashed -ml-[1px]" />
+    </div>
+    <div class="flex flex-col grow">
+      <div class="text-sm text-neutral-900 font-medium">{{ title }}</div>
+      <div class="text-xs text-neutral-400">{{ description }}</div>
+    </div>
+    <div class="flex gap-2 items-center text-sm h-6 px-2.5 py-1 rounded-full" :class="valueClass">
+      <lfx-icon :name="itemValue.changeType === 'positive' ? 'thumbs-up' : 'thumbs-down'" :size="14" />
+      {{ itemValue.value }}
+      {{ itemValue.unit }}
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { MergeLeadTimeItem } from '../types/merge-lead-time.types';
+import LfxIcon from '~/components/uikit/icon/icon.vue';
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    description: string;
+    icon: string;
+    itemValue: MergeLeadTimeItem;
+    isLast?: boolean;
+  }>(),
+  {
+    isLast: false
+  }
+);
+
+const valueClass = computed(//
+  () => (props.itemValue.changeType === 'positive'
+    ? 'bg-positive-50 text-positive-600'
+    : 'bg-negative-50 text-negative-600')
+);
+</script>
--- frontend/app/components/modules/project/components/development/merge-lead-time.vue
+++ frontend/app/components/modules/project/components/development/merge-lead-time.vue
@@ -3,15 +3,83 @@
     <h3 class="text-heading-3 font-semibold font-secondary pb-3">
       Merge lead time
     </h3>
-    <p class="text-body-2 text-neutral-500">
+    <p class="text-body-2 text-neutral-500 mb-6">
       Active contributor is an individual performing tasks such as commits,
       issues, or pull requests during the selected time period.
     </p>
+    <hr>
+    <section class="mt-5">
+      <div v-if="status === 'success'" class="flex flex-row gap-4 items-center mb-8">
+        <div class="text-data-display-1">{{ formatNumber(summary.current) }} days</div>
+        <lfx-delta-display :summary="summary" icon="circle-arrow-up-right" icon-type="solid" unit="d" />
+      </div>
+
+      <div class="w-full min-h-[250px] mt-5">
+        <div v-if="status === 'success'" class="flex flex-col gap-10 pt-6">
+          <lfx-merge-lead-item
+            title="Pickup" description="Pull request raised" icon="code-pull-request"
+            :item-value="mergeLeadTime.data.pickup" />
+          <lfx-merge-lead-item
+            title="Review" description="Review started" icon="eye"
+            :item-value="mergeLeadTime.data.review" />
+          <lfx-merge-lead-item
+            title="Accepted" description="Pull request accepted" icon="check-circle"
+            :item-value="mergeLeadTime.data.accepted" />
+          <lfx-merge-lead-item
+            title="Merged" description="" icon="thumbs-up" :item-value="mergeLeadTime.data.pickup"
+            is-last />
+        </div>
+        <lfx-spinner v-else />
+      </div>
+    </section>
   </lfx-card>
 </template>
 
 <script setup lang="ts">
-import LfxCard from "~/components/uikit/card/card.vue";
+import { useFetch, useRoute } from 'nuxt/app';
+import { computed, watch } from 'vue';
+import type { MergeLeadTime } from './types/merge-lead-time.types';
+import LfxMergeLeadItem from './fragments/merge-lead-item.vue';
+import LfxCard from '~/components/uikit/card/card.vue';
+import LfxDeltaDisplay from '~/components/uikit/delta-display/delta-display.vue';
+import useToastService from '~/components/uikit/toast/toast.service';
+import { ToastTypesEnum } from '~/components/uikit/toast/types/toast.types';
+import LfxSpinner from '~/components/uikit/spinner/spinner.vue';
+import { formatNumber } from '~/components/shared/utils/formatter';
+import type { Summary } from '~/components/shared/types/summary.types';
+
+const props = withDefaults(
+  defineProps<{
+    timePeriod?: string;
+  }>(),
+  {
+    timePeriod: '90d'
+  }
+);
+
+const { showToast } = useToastService();
+
+const route = useRoute();
+
+const { data, status, error } = useFetch(
+  () => `/api/projects/development/merge-lead-time?project=${route.params.slug
+    }&repository=${route.params.name || ''}&time-period=${props.timePeriod}`
+);
+
+const mergeLeadTime = computed<MergeLeadTime>(() => data.value as MergeLeadTime);
+
+const summary = computed<Summary>(() => mergeLeadTime.value.summary);
+
+watch(error, (err) => {
+  if (err) {
+    showToast(
+      `Error fetching merge lead time: ${error.value?.statusMessage}`,
+      ToastTypesEnum.negative,
+      undefined,
+      10000
+    );
+  }
+});
 </script>
 
 <script lang="ts">
