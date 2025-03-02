c2abebb1e2eb31e0911c4b3472a319ea1815868e
Gašper Grom <gasper.grom@gmail.com>
2025-03-02 14:21:38 +0100

    Change backend format
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/modules/project/config/date-options.ts
+++ frontend/app/components/modules/project/config/date-options.ts
@@ -14,22 +14,22 @@ export const lfxProjectDateOptionsPast: DateOptionConfig[] = [
     {
         key: 'past90days',
         label: 'Past 90 days',
-        startDate: date().subtract(90, 'day').startOf('day').toISOString(),
-        endDate: date().endOf('day').toISOString(),
+        startDate: date().subtract(90, 'day').startOf('day').format('YYYY-MM-DD'),
+        endDate: date().endOf('day').format('YYYY-MM-DD'),
         description: `${date().subtract(90, 'day').format('MMM D, YYYY')} -> Today`
     },
     {
         key: 'past180days',
         label: 'Past 180 days',
-        startDate: date().subtract(180, 'day').startOf('day').toISOString(),
-        endDate: date().endOf('day').toISOString(),
+        startDate: date().subtract(180, 'day').startOf('day').format('YYYY-MM-DD'),
+        endDate: date().endOf('day').format('YYYY-MM-DD'),
         description: `${date().subtract(180, 'day').format('MMM D, YYYY')} -> Today`
     },
     {
         key: 'past365days',
         label: 'Past 365 days',
-        startDate: date().subtract(365, 'day').startOf('day').toISOString(),
-        endDate: date().endOf('day').toISOString(),
+        startDate: date().subtract(365, 'day').startOf('day').format('YYYY-MM-DD'),
+        endDate: date().endOf('day').format('YYYY-MM-DD'),
         description: `${date().subtract(365, 'day').format('MMM D, YYYY')} -> Today`
     },
 ];
@@ -38,31 +38,31 @@ export const lfxProjectDateOptionsPrevious: DateOptionConfig[] = [
     {
         key: 'previousQuarter',
         label: 'Previous quarter',
-        startDate: date().subtract(1, 'quarter').startOf('quarter').toISOString(),
-        endDate: date().subtract(1, 'quarter').endOf('quarter').toISOString(),
+        startDate: date().subtract(1, 'quarter').startOf('quarter').format('YYYY-MM-DD'),
+        endDate: date().subtract(1, 'quarter').endOf('quarter').format('YYYY-MM-DD'),
         description: `${date().subtract(1, 'quarter').startOf('quarter').format('MMM YYYY')} 
         -> ${date().subtract(1, 'quarter').endOf('quarter').format('MMM YYYY')}`
     },
     {
         key: 'previousYear',
         label: 'Previous year',
-        startDate: date().subtract(1, 'year').startOf('year').toISOString(),
-        endDate: date().subtract(1, 'year').endOf('year').toISOString(),
+        startDate: date().subtract(1, 'year').startOf('year').format('YYYY-MM-DD'),
+        endDate: date().subtract(1, 'year').endOf('year').format('YYYY-MM-DD'),
         description: `${date().subtract(1, 'year').format('YYYY')}`
     },
     {
         key: 'previous5Year',
         label: 'Previous 5 years',
-        startDate: date().subtract(5, 'year').startOf('year').toISOString(),
-        endDate: date().subtract(1, 'year').endOf('year').toISOString(),
+        startDate: date().subtract(5, 'year').startOf('year').format('YYYY-MM-DD'),
+        endDate: date().subtract(1, 'year').endOf('year').format('YYYY-MM-DD'),
         description: `${date().subtract(5, 'year').format('YYYY')} 
         -> ${date().subtract(1, 'year').endOf('year').format('YYYY')}`
     },
     {
         key: 'previous10Year',
         label: 'Previous 10 years',
-        startDate: date().subtract(10, 'year').startOf('year').toISOString(),
-        endDate: date().subtract(1, 'year').endOf('year').toISOString(),
+        startDate: date().subtract(10, 'year').startOf('year').format('YYYY-MM-DD'),
+        endDate: date().subtract(1, 'year').endOf('year').format('YYYY-MM-DD'),
         description: `${date().subtract(10, 'year').format('YYYY')} 
         -> ${date().subtract(1, 'year').endOf('year').format('YYYY')}`
     },
