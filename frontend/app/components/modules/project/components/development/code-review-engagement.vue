791d91f84d8228e22fe860d5d3cb6bc664a84e64
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-03-04 13:48:51 +0800

    Improvement/wait time 1st review (#64)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/components/development/code-review-engagement.vue
+++ frontend/app/components/modules/project/components/development/code-review-engagement.vue
@@ -3,15 +3,101 @@
     <h3 class="text-heading-3 font-semibold font-secondary pb-3">
       Code review engagement
     </h3>
-    <p class="text-body-2 text-neutral-500">
+    <p class="text-body-2 text-neutral-500 mb-6">
       Active contributor is an individual performing tasks such as commits,
       issues, or pull requests during the selected time period.
     </p>
+    <hr>
+    <section class="mt-5">
+      <lfx-tabs
+        :tabs="tabs"
+        :model-value="activeTab"
+        @update:model-value="activeTab = $event"
+      />
+      <div
+        v-if="status === 'success'"
+        class="flex flex-row gap-4 items-center mt-7 mb-8"
+      >
+        <div class="text-data-display-1">{{ formatNumber(summary.current) }} contributors</div>
+        <lfx-delta-display
+          :summary="summary"
+          icon="circle-arrow-up-right"
+          icon-type="solid"
+        />
+      </div>
+      <div class="w-full h-[330px]">
+        <div v-if="status === 'success'">
+          <div class="font-semibold mb-5 mt-8">
+            <span class="text-black">Top contributors </span>
+          </div>
+
+          <lfx-code-review-table
+            show-percentage
+            :metric="activeTab"
+            :code-review-item="codeReviewEngagement.data"
+          />
+        </div>
+        <lfx-spinner v-else />
+      </div>
+    </section>
   </lfx-card>
 </template>
 
 <script setup lang="ts">
-import LfxCard from "~/components/uikit/card/card.vue";
+import { useFetch, useRoute } from 'nuxt/app';
+import { ref, computed, watch } from 'vue';
+import { storeToRefs } from "pinia";
+import type { CodeReviewEngagement } from './types/code-review-engagement.types';
+import LfxCodeReviewTable from './fragments/code-review-table.vue';
+import type { Summary } from '~/components/shared/types/summary.types';
+import LfxCard from '~/components/uikit/card/card.vue';
+import LfxDeltaDisplay from '~/components/uikit/delta-display/delta-display.vue';
+import LfxTabs from '~/components/uikit/tabs/tabs.vue';
+import useToastService from '~/components/uikit/toast/toast.service';
+import { ToastTypesEnum } from '~/components/uikit/toast/types/toast.types';
+import LfxSpinner from '~/components/uikit/spinner/spinner.vue';
+import { formatNumber } from '~/components/shared/utils/formatter';
+import { useProjectStore } from "~/components/modules/project/store/project.store";
+
+const { showToast } = useToastService();
+
+const { startDate, endDate } = storeToRefs(useProjectStore());
+
+const activeTab = ref('pr-participants');
+const route = useRoute();
+
+const { data, status, error } = useFetch(
+  () => `/api/project/${route.params.slug}/development/code-review-engagement`,
+  {
+    params: {
+      metric: activeTab.value,
+      repository: route.params.name || '',
+      startDate,
+      endDate,
+    }
+  }
+);
+
+const codeReviewEngagement = computed<CodeReviewEngagement>(() => data.value as CodeReviewEngagement);
+
+const summary = computed<Summary>(() => codeReviewEngagement.value.summary);
+
+const tabs = [
+  { label: 'PR participants', value: 'pr-participants' },
+  { label: 'Review comments', value: 'review-comments' },
+  { label: 'Code reviews', value: 'code-reviews' }
+];
+
+watch(error, (err) => {
+  if (err) {
+    showToast(
+      `Error fetching code review engagement: ${error.value?.statusMessage}`,
+      ToastTypesEnum.negative,
+      undefined,
+      10000
+    );
+  }
+});
 </script>
 
 <script lang="ts">
---
+++ frontend/server/mocks/wait-time-1st-review.mock.ts
@@ -0,0 +1,92 @@
+export const waitTime1stReview = {
+  summary: {
+    current: 3632, // in minutes
+    previous: 3608, // in minutes
+    percentageChange: 2.3,
+    changeValue: 24,
+    periodFrom: '2024-11-11T00:00:00Z',
+    periodTo: '2025-02-12T00:00:00Z'
+  },
+  data: [
+    {
+      dateFrom: '2024-11-11T00:00:00Z',
+      dateTo: '2024-11-18T00:00:00Z',
+      waitTime: 89 // in hours
+    },
+    {
+      dateFrom: '2024-11-18T00:00:00Z',
+      dateTo: '2024-11-25T00:00:00Z',
+      waitTime: 60
+    },
+    {
+      dateFrom: '2024-11-25T00:00:00Z',
+      dateTo: '2024-12-02T00:00:00Z',
+      waitTime: 88
+    },
+    {
+      dateFrom: '2024-12-02T00:00:00Z',
+      dateTo: '2024-12-09T00:00:00Z',
+      waitTime: 51
+    },
+    {
+      dateFrom: '2024-12-09T00:00:00Z',
+      dateTo: '2024-12-16T00:00:00Z',
+      waitTime: 89
+    },
+    {
+      dateFrom: '2024-12-16T00:00:00Z',
+      dateTo: '2024-12-23T00:00:00Z',
+      waitTime: 52
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      waitTime: 90
+    },
+    {
+      dateFrom: '2024-12-23T00:00:00Z',
+      dateTo: '2025-01-06T00:00:00Z',
+      waitTime: 73
+    },
+    {
+      dateFrom: '2025-01-06T00:00:00Z',
+      dateTo: '2025-01-13T00:00:00Z',
+      waitTime: 51
+    },
+    {
+      dateFrom: '2025-01-13T00:00:00Z',
+      dateTo: '2025-01-20T00:00:00Z',
+      waitTime: 94
+    },
+    {
+      dateFrom: '2025-01-20T00:00:00Z',
+      dateTo: '2025-01-27T00:00:00Z',
+      waitTime: 92
+    },
+    {
+      dateFrom: '2025-01-27T00:00:00Z',
+      dateTo: '2025-02-03T00:00:00Z',
+      waitTime: 45
+    },
+    {
+      dateFrom: '2025-02-03T00:00:00Z',
+      dateTo: '2025-02-10T00:00:00Z',
+      waitTime: 33
+    },
+    {
+      dateFrom: '2025-02-10T00:00:00Z',
+      dateTo: '2025-02-17T00:00:00Z',
+      waitTime: 66
+    },
+    {
+      dateFrom: '2025-02-17T00:00:00Z',
+      dateTo: '2025-02-24T00:00:00Z',
+      waitTime: 44
+    },
+    {
+      dateFrom: '2025-02-24T00:00:00Z',
+      dateTo: '2025-03-03T00:00:00Z',
+      waitTime: 97
+    }
+  ]
+};
