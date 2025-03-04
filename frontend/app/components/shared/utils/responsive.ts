3079c7e38cd55c5d10624e5e1655da7fdee62f97
Gašper Grom <gasper.grom@gmail.com>
2025-03-04 06:58:45 +0100

    Collection list mobile (#65)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

---
+++ frontend/app/components/shared/utils/responsive.ts
@@ -0,0 +1,26 @@
+import {
+ onMounted, onUnmounted, ref
+} from 'vue';
+
+const useResponsive = () => {
+  const pageWidth = ref(0);
+
+  const updatePageWidth = () => {
+    pageWidth.value = window.innerWidth;
+  };
+
+  onMounted(() => {
+    updatePageWidth();
+    window.addEventListener('resize', updatePageWidth);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', updatePageWidth);
+  });
+
+  return {
+    pageWidth,
+  };
+};
+
+export default useResponsive;
