b8042bed1362a1b1e9a444210520e89ed0c74881
Gašper Grom <gasper.grom@gmail.com>
2025-03-10 14:53:11 +0000

    Add seo meta data
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/head.ts
+++ frontend/setup/head.ts
@@ -1,27 +1,52 @@
+const title = 'LFX Insights | Enhance data-driven collaboration management';
+const description = 'Unlock key insights with LFX Insights – a powerful tool for open-source projects, '
+    + 'providing contributor analytics, popularity trends, development activity, and security insights to '
+    + 'enhance collaboration and decision-making.';
+
 export default {
-    title: 'LFX Insights',
+    title,
     htmlAttrs: {
         lang: 'en', // Language of the website
     },
     meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Insights on opensource projects' },
+        {
+            hid: 'description',
+            name: 'description',
+            content: description
+        },
         { hid: 'keywords', name: 'keywords', content: 'lfx, insights, opensource' },
         { hid: 'author', name: 'author', content: 'The Linux Foundation' },
         { hid: 'theme-color', name: 'theme-color', content: '#ffffff' }, // Browser theme color
-        { hid: 'og:title', property: 'og:title', content: 'LFX Insights' },
-        { hid: 'og:description', property: 'og:description', content: 'Insights on opensource projects' },
+        {
+            hid: 'og:title',
+            property: 'og:title',
+            content: title
+        },
+        {
+            hid: 'og:description',
+            property: 'og:description',
+            content: description
+        },
         { hid: 'og:type', property: 'og:type', content: 'website' },
-        { hid: 'og:image', property: 'og:image', content: '/favicon.ico' },
+        { hid: 'og:image', property: 'og:image', content: 'https://cdn.platform.linuxfoundation.org/assets/lf-favicon.png' },
         { hid: 'og:url', property: 'og:url', content: 'https://insights.lfx.org' },
         { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-        { hid: 'twitter:title', name: 'twitter:title', content: 'LFX Insights' },
-        { hid: 'twitter:description', name: 'twitter:description', content: 'Insights on opensource projects' },
-        { hid: 'twitter:image', name: 'twitter:image', content: '/favicon.ico' }
+        {
+            hid: 'twitter:title',
+            name: 'twitter:title',
+            content: title
+        },
+        {
+            hid: 'twitter:description',
+            name: 'twitter:description',
+            content: description
+        },
+        { hid: 'twitter:image', name: 'twitter:image', content: 'https://cdn.platform.linuxfoundation.org/assets/lf-favicon.png' }
     ],
     link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: 'https://cdn.platform.linuxfoundation.org/assets/lf-favicon.png' },
         {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
         {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous'},
         {
