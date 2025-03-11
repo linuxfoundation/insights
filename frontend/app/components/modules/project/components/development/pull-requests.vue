3771fe9636dda6a83da8f595864ba62134156574
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-03-11 18:43:25 +0800

    Improvement/custom time granularity (#92)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/components/development/pull-requests.vue
+++ frontend/app/components/modules/project/components/development/pull-requests.vue
@@ -106,14 +106,18 @@ import { useProjectStore } from "~/components/modules/project/store/project.stor
 import LfxIcon from "~/components/uikit/icon/icon.vue";
 import { isEmptyData } from '~/components/shared/utils/helper';
 import { barGranularities } from '~/components/shared/types/granularity';
+import { dateOptKeys } from '~/components/modules/project/config/date-options';
+import type { Granularity } from '~~/types/shared/granularity';
 
 const {
-  startDate, endDate, selectedRepository, selectedTimeRangeKey
+  startDate, endDate, selectedRepository, selectedTimeRangeKey, customRangeGranularity
 } = storeToRefs(useProjectStore())
 
 const route = useRoute();
 
-const granularity = computed(() => barGranularities[selectedTimeRangeKey.value as keyof typeof barGranularities]);
+const granularity = computed(() => (selectedTimeRangeKey.value === dateOptKeys.custom
+  ? customRangeGranularity.value[0] as Granularity
+  : barGranularities[selectedTimeRangeKey.value as keyof typeof barGranularities]));
 const { data, status, error } = useFetch(
   () => `/api/project/${route.params.slug}/development/pull-requests`,
   {
