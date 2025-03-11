002a1e6e8e29a64bbdfa4646c4bc336702e49d5d
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-03-11 18:28:07 +0800

    Improvement/contributors types (#89)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>
    Signed-off-by: emlimlf <52259294+emlimlf@users.noreply.github.com>

--- frontend/server/mocks/retention.mock.ts
+++ frontend/server/mocks/retention.mock.ts
@@ -1,105 +1,105 @@
 export const contributorRetention = [
   {
-    dateFrom: '2024-11-11T00:00:00Z',
-    dateTo: '2024-11-18T00:00:00Z',
+    startDate: '2024-11-11T00:00:00Z',
+    endDate: '2024-11-18T00:00:00Z',
     percentage: 82
   },
   {
-    dateFrom: '2024-11-18T00:00:00Z',
-    dateTo: '2024-11-25T00:00:00Z',
+    startDate: '2024-11-18T00:00:00Z',
+    endDate: '2024-11-25T00:00:00Z',
     percentage: 68
   },
   {
-    dateFrom: '2024-11-25T00:00:00Z',
-    dateTo: '2024-12-02T00:00:00Z',
+    startDate: '2024-11-25T00:00:00Z',
+    endDate: '2024-12-02T00:00:00Z',
     percentage: 55
   },
   {
-    dateFrom: '2024-12-02T00:00:00Z',
-    dateTo: '2024-12-09T00:00:00Z',
+    startDate: '2024-12-02T00:00:00Z',
+    endDate: '2024-12-09T00:00:00Z',
     percentage: 42
   },
   {
-    dateFrom: '2024-12-09T00:00:00Z',
-    dateTo: '2024-12-16T00:00:00Z',
+    startDate: '2024-12-09T00:00:00Z',
+    endDate: '2024-12-16T00:00:00Z',
     percentage: 30
   },
   {
-    dateFrom: '2024-12-16T00:00:00Z',
-    dateTo: '2024-12-23T00:00:00Z',
+    startDate: '2024-12-16T00:00:00Z',
+    endDate: '2024-12-23T00:00:00Z',
     percentage: 20
   },
   {
-    dateFrom: '2024-12-23T00:00:00Z',
-    dateTo: '2024-12-30T00:00:00Z',
+    startDate: '2024-12-23T00:00:00Z',
+    endDate: '2024-12-30T00:00:00Z',
     percentage: 10
   },
   {
-    dateFrom: '2024-12-30T00:00:00Z',
-    dateTo: '2025-01-06T00:00:00Z',
+    startDate: '2024-12-30T00:00:00Z',
+    endDate: '2025-01-06T00:00:00Z',
     percentage: 5
   },
   {
-    dateFrom: '2025-01-06T00:00:00Z',
-    dateTo: '2025-01-13T00:00:00Z',
+    startDate: '2025-01-06T00:00:00Z',
+    endDate: '2025-01-13T00:00:00Z',
     percentage: 2
   },
   {
-    dateFrom: '2025-01-13T00:00:00Z',
-    dateTo: '2025-01-20T00:00:00Z',
+    startDate: '2025-01-13T00:00:00Z',
+    endDate: '2025-01-20T00:00:00Z',
     percentage: 1
   }
 ];
 
 export const organizationRetention = [
   {
-    dateFrom: '2024-11-11T00:00:00Z',
-    dateTo: '2024-11-18T00:00:00Z',
+    startDate: '2024-11-11T00:00:00Z',
+    endDate: '2024-11-18T00:00:00Z',
     percentage: 99
   },
   {
-    dateFrom: '2024-11-18T00:00:00Z',
-    dateTo: '2024-11-25T00:00:00Z',
+    startDate: '2024-11-18T00:00:00Z',
+    endDate: '2024-11-25T00:00:00Z',
     percentage: 98
   },
   {
-    dateFrom: '2024-11-25T00:00:00Z',
-    dateTo: '2024-12-02T00:00:00Z',
+    startDate: '2024-11-25T00:00:00Z',
+    endDate: '2024-12-02T00:00:00Z',
     percentage: 89
   },
   {
-    dateFrom: '2024-12-02T00:00:00Z',
-    dateTo: '2024-12-09T00:00:00Z',
+    startDate: '2024-12-02T00:00:00Z',
+    endDate: '2024-12-09T00:00:00Z',
     percentage: 80
   },
   {
-    dateFrom: '2024-12-09T00:00:00Z',
-    dateTo: '2024-12-16T00:00:00Z',
+    startDate: '2024-12-09T00:00:00Z',
+    endDate: '2024-12-16T00:00:00Z',
     percentage: 70
   },
   {
-    dateFrom: '2024-12-16T00:00:00Z',
-    dateTo: '2024-12-23T00:00:00Z',
+    startDate: '2024-12-16T00:00:00Z',
+    endDate: '2024-12-23T00:00:00Z',
     percentage: 60
   },
   {
-    dateFrom: '2024-12-23T00:00:00Z',
-    dateTo: '2024-12-30T00:00:00Z',
+    startDate: '2024-12-23T00:00:00Z',
+    endDate: '2024-12-30T00:00:00Z',
     percentage: 20
   },
   {
-    dateFrom: '2024-12-30T00:00:00Z',
-    dateTo: '2025-01-06T00:00:00Z',
+    startDate: '2024-12-30T00:00:00Z',
+    endDate: '2025-01-06T00:00:00Z',
     percentage: 10
   },
   {
-    dateFrom: '2025-01-06T00:00:00Z',
-    dateTo: '2025-01-13T00:00:00Z',
+    startDate: '2025-01-06T00:00:00Z',
+    endDate: '2025-01-13T00:00:00Z',
     percentage: 5
   },
   {
-    dateFrom: '2025-01-13T00:00:00Z',
-    dateTo: '2025-01-20T00:00:00Z',
+    startDate: '2025-01-13T00:00:00Z',
+    endDate: '2025-01-20T00:00:00Z',
     percentage: 1
   }
 ];
---
+++ frontend/types/contributors/responses.types.ts
@@ -0,0 +1,68 @@
+import type { Summary, Meta } from '../shared/summary.types';
+
+export interface ActiveContributors {
+  summary: Summary;
+  data: {
+    startDate: string;
+    endDate: string;
+    contributors: number;
+  }[];
+}
+
+export interface ActiveOrganizations {
+  summary: Summary;
+  data: {
+    startDate: string;
+    endDate: string;
+    organizations: number;
+  }[];
+}
+
+export interface Contributor {
+  avatar: string;
+  name: string;
+  contributions: number;
+  percentage?: number;
+  email: string;
+}
+
+export interface Organization {
+  logo: string;
+  name: string;
+  contributions: number;
+  percentage?: number;
+  website: string;
+}
+
+export interface ContributorLeaderboard {
+  meta: Meta;
+  data: Contributor[];
+}
+
+export interface OrganizationLeaderboard {
+  meta: Meta;
+  data: Organization[];
+}
+
+export interface Dependency {
+  count: number;
+  percentage: number;
+}
+
+export interface ContributorDependency {
+  topContributors: Dependency;
+  otherContributors: Dependency;
+  list: Contributor[];
+}
+
+export interface OrganizationDependency {
+  topOrganizations: Dependency;
+  otherOrganizations: Dependency;
+  list: Organization[];
+}
+
+export interface Retention {
+  startDate: string;
+  endDate: string;
+  percentage: number;
+}
