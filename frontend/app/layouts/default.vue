c4d5c67599bcb49b14446cdb93b3d2cc7d4fdc45
Gašper Grom <gasper.grom@gmail.com>
2025-03-12 12:21:51 +0000

    Remove footer
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -3,11 +3,11 @@
     <lfx-navbar />
     <slot />
     <lfx-toast theme="dark" />
-    <lfx-footer class="px-10 mx-auto max-w-3xl pb-6 pt-6" />
+    <!--    <lfx-footer class="px-10 mx-auto max-w-3xl pb-6 pt-6" />-->
   </main>
 </template>
 <script setup lang="ts">
 import LfxNavbar from '~/components/shared/layout/navbar.vue';
 import LfxToast from '~/components/uikit/toast/toast.vue';
-import '@linuxfoundation/lfx-ui-core';
+// import '@linuxfoundation/lfx-ui-core';
 </script>
