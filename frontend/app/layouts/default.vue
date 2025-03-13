a4cf95ca86c2d78bb07c7d3927fcb595043fde97
Gašper Grom <gasper.grom@gmail.com>
2025-03-13 21:05:37 +0000

    Footer error implementation (#101)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>
    Signed-off-by: Asitha de Silva <asithade@gmail.com>
    Co-authored-by: Asitha de Silva <asithade@gmail.com>

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -3,14 +3,19 @@
     <lfx-navbar />
     <slot />
     <lfx-toast theme="dark" />
+    <client-only>
+      <lfx-footer class="px-10 mx-auto max-w-3xl pb-6 pt-6" />
+    </client-only>
   </main>
-<!--  <lfx-footer />-->
 </template>
 
 <script setup lang="ts">
 import LfxNavbar from '~/components/shared/layout/navbar.vue';
 import LfxToast from '~/components/uikit/toast/toast.vue';
-// import '@linuxfoundation/lfx-ui-core';
+// Only import the UI core library on the client side
+if (import.meta.client) {
+  await import('@linuxfoundation/lfx-ui-core');
+}
 </script>
 
 <style lang="scss" scoped>
