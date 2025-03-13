9fea663074f3bec07ee9f9c9a7c3f9c09198e56d
Gašper Grom <gasper.grom@gmail.com>
2025-03-13 07:42:36 +0000

    Remove custom footer
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -3,35 +3,6 @@
     <lfx-navbar />
     <slot />
     <lfx-toast theme="dark" />
-    <div class="px-10 mx-auto max-w-3xl pb-6 pt-6">
-      <div class="copyright-container">
-        <p
-          class="copyright text-xs text-center"
-        >
-          Copyright © 2025 The Linux Foundation®. All rights reserved.
-          The Linux Foundation has registered trademarks and uses trademarks. For more details, including terms of use,
-          <a
-            href="https://www.linuxfoundation.org/legal/platform-use-agreement/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Platform Usage</a>,
-          <a
-            href="https://www.linuxfoundation.org/legal/privacy-policy?hsLang=en"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Privacy Policy</a>,
-          and <a
-            href="https://www.linuxfoundation.org/legal/trademark-usage?hsLang=en"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Trademark Usage</a>,
-          please see our <a
-            href="https://www.linuxfoundation.org/legal/policies"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Policies</a> page.</p>
-      </div>
-    </div>
   </main>
 <!--  <lfx-footer />-->
 </template>
