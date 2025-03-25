01163c67f559ed9c27b80f1cd82279d9ee8943f7
Gašper Grom <gasper.grom@gmail.com>
2025-03-25 09:53:24 +0100

    Migrate tooltip to popover (#130)

--- frontend/app/components/shared/layout/menu/menu-tablet.vue
+++ frontend/app/components/shared/layout/menu/menu-tablet.vue
@@ -15,52 +15,54 @@
       </lfx-menu-button>
 
       <template #content>
-        <div class="p-3 w-80">
-          <div class="flex flex-col gap-3 items-start border-b border-neutral-100 pb-3">
-            <lfx-menu-static-links />
-          </div>
-          <div class="pt-4">
-            <p class="text-xs font-semibold text-neutral-400 pl-3 pb-3">
-              LFX tools
-            </p>
-            <div class="flex flex-col gap-1">
-              <a
-                v-for="(tool, key) in lfxTools"
-                :key="key"
-                :href="tool.link"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="bg-white hover:bg-neutral-50 transition w-full px-3 py-2 flex items-center rounded-md"
-              >
-                <lfx-icon
-                  :name="tool.icon"
-                  type="duotone"
-                  class="text-[#00548F]"
-                  :size="16"
-                />
-                <div class="pl-3">
-                  <h6 class="text-xs font-medium leading-5">{{tool.name}}</h6>
-                </div>
-              </a>
+        <div class="bg-white shadow-lg rounded-lg border border-neutral-200">
+          <div class="p-3 w-80">
+            <div class="flex flex-col gap-3 items-start border-b border-neutral-100 pb-3">
+              <lfx-menu-static-links />
+            </div>
+            <div class="pt-4">
+              <p class="text-xs font-semibold text-neutral-400 pl-3 pb-3">
+                LFX tools
+              </p>
+              <div class="flex flex-col gap-1">
+                <a
+                  v-for="(tool, key) in lfxTools"
+                  :key="key"
+                  :href="tool.link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="bg-white hover:bg-neutral-50 transition w-full px-3 py-2 flex items-center rounded-md"
+                >
+                  <lfx-icon
+                    :name="tool.icon"
+                    type="duotone"
+                    class="text-[#00548F]"
+                    :size="16"
+                  />
+                  <div class="pl-3">
+                    <h6 class="text-xs font-medium leading-5">{{tool.name}}</h6>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="py-2 px-3 bg-neutral-100 flex justify-center">
-          <a
-            :href="lfxMenu.footer.href"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="flex items-center gap-1.5"
-          >
-            <p class="text-xs font-medium text-neutral-500">
-              {{ lfxMenu.footer.label }}
-            </p>
-            <lfx-icon
-              name="arrow-up-right"
-              class="text-neutral-500"
-              :size="14"
-            />
-          </a>
+          <div class="py-2 px-3 bg-neutral-100 flex justify-center">
+            <a
+              :href="lfxMenu.footer.href"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="flex items-center gap-1.5"
+            >
+              <p class="text-xs font-medium text-neutral-500">
+                {{ lfxMenu.footer.label }}
+              </p>
+              <lfx-icon
+                name="arrow-up-right"
+                class="text-neutral-500"
+                :size="14"
+              />
+            </a>
+          </div>
         </div>
       </template>
     </lfx-popover>
