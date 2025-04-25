d518b02027e2cd51297c32279e5a52340620c931
Gašper Grom <gasper.grom@gmail.com>
2025-04-25 07:51:03 +0200

    Report data fixes
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/modules/widget/components/shared/widget-menu-item.vue
+++ frontend/app/components/modules/widget/components/shared/widget-menu-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-9 w-9 block rounded-full hover:bg-neutral-100 flex items-center justify-center cursor-pointer transition-all">
+  <div class="h-9 w-9 rounded-full hover:bg-neutral-100 flex items-center justify-center cursor-pointer transition-all">
     <slot />
   </div>
 </template>
