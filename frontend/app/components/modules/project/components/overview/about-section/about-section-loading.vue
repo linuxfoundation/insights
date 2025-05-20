3b8a5c4c45dc5ca0b1efbbf34496ef5e811b4074
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-05-20 16:46:22 +0800

    used skeleton loading for the about section (#263)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/app/components/modules/project/components/overview/about-section/about-section-loading.vue
@@ -0,0 +1,41 @@
+<!--
+Copyright (c) 2025 The Linux Foundation and each contributor.
+SPDX-License-Identifier: MIT
+-->
+<template>
+  <slot v-if="!isLoading" />
+  <div
+    v-else
+    class="flex flex-col gap-3"
+  >
+    <lfx-skeleton
+      height="1rem"
+      width="7.5rem"
+      class="rounded-sm"
+    />
+    <lfx-skeleton
+      height="1rem"
+      width="100%"
+      class="rounded-sm"
+    />
+    <lfx-skeleton
+      height="1rem"
+      width="16.875rem"
+      class="rounded-sm"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LfxSkeleton from '~/components/uikit/skeleton/skeleton.vue';
+
+defineProps<{
+  isLoading: boolean;
+}>();
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfxProjectAboutSectionLoading'
+};
+</script>
