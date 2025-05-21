04f7ef70c325f76fed81b16b8771ace556632e61
joanagmaia <jmaia@contractor.linuxfoundation.org>
2025-05-21 13:15:00 +0100

    fix: remove contributors benchmark and fix lorem ipsum docs (#277)
    Signed-off-by: Joana Maia <jmaia@contractor.linuxfoundation.org>

--- frontend/app/components/modules/project/components/overview/score-details/score-list.vue
+++ frontend/app/components/modules/project/components/overview/score-details/score-list.vue
@@ -4,11 +4,11 @@ SPDX-License-Identifier: MIT
 -->
 <template>
   <div
-    v-if="data"
+    v-if="parsedData"
     class="flex flex-col gap-4"
   >
     <div
-      v-for="item in data"
+      v-for="item in parsedData"
       :key="item.benchmarkKey"
       class="[&:not(:last-child)]:border-b border-neutral-100 [&:not(:last-child)]:pb-4"
     >
@@ -21,12 +21,18 @@ SPDX-License-Identifier: MIT
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import LfxScoreItem from './score-item.vue';
-import type { ScoreData } from '~~/types/shared/benchmark.types';
+import { BenchmarkKeys, type ScoreData } from '~~/types/shared/benchmark.types';
 
-defineProps<{
+const props = defineProps<{
   data: ScoreData[] | undefined;
 }>();
+
+// NOTE: TEMPORARY
+const parsedData = computed(() => props.data?.filter(
+  (item) => item.benchmarkKey !== BenchmarkKeys.GeographicalDistribution
+));
 
 </script>
 <script lang="ts">
