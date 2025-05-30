45ed8cbe272e83668b761c90bb8561397ca04b8e
Gašper Grom <gasper.grom@gmail.com>
2025-05-30 07:03:45 -0700

    fix: software value overview (#313)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/modules/project/components/overview/about-section/about-software-value.vue
+++ frontend/app/components/modules/project/components/overview/about-section/about-software-value.vue
@@ -23,7 +23,6 @@ SPDX-License-Identifier: MIT
     </div>
   </div>
   <div
-    v-if="project?.softwareValue"
     class="flex flex-col gap-2 text-xs"
   >
     <div class="text-neutral-400 font-semibold flex items-center gap-1">
@@ -36,54 +35,55 @@ SPDX-License-Identifier: MIT
       </lfx-tooltip>
     </div>
     <div class="text-neutral-900">
-      ${{ formatNumberShort(project?.softwareValue) }}
-    </div>
-  </div>
-  <div
-    v-if="tmpShowFlag"
-    class="flex flex-col gap-3 text-xs"
-  >
-    <div class="text-neutral-400 font-semibold flex items-center gap-1">
-      Maturity status
-      <lfx-tooltip
-        content="Maturity status"
-        placement="top"
-      >
-        <lfx-icon name="question-circle" />
-      </lfx-tooltip>
-    </div>
-    <div>
-      <lfx-tag
-        v-if="project?.maturityStatus"
-        :size="'small'"
-        :style="'info'"
-        type="outline"
-        class="justify-center"
-      >
-        {{ project?.maturityStatus }}
-      </lfx-tag>
-      <span
-        v-else
-        class="text-neutral-900"
-      >
+      <span v-if="project?.softwareValue">
+        ${{ formatNumberShort(project?.softwareValue) }}
+      </span>
+      <span v-else>
         No Data
       </span>
     </div>
-
   </div>
+<!--  <div-->
+<!--    class="flex flex-col gap-3 text-xs"-->
+<!--  >-->
+<!--    <div class="text-neutral-400 font-semibold flex items-center gap-1">-->
+<!--      Maturity status-->
+<!--      <lfx-tooltip-->
+<!--        content="Maturity status"-->
+<!--        placement="top"-->
+<!--      >-->
+<!--        <lfx-icon name="question-circle" />-->
+<!--      </lfx-tooltip>-->
+<!--    </div>-->
+<!--    <div>-->
+<!--      <lfx-tag-->
+<!--        v-if="project?.maturityStatus"-->
+<!--        size="small"-->
+<!--        variation="info"-->
+<!--        type="outline"-->
+<!--        class="justify-center"-->
+<!--      >-->
+<!--        {{ project?.maturityStatus }}-->
+<!--      </lfx-tag>-->
+<!--      <span-->
+<!--        v-else-->
+<!--        class="text-neutral-900"-->
+<!--      >-->
+<!--        No Data-->
+<!--      </span>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import {DateTime} from 'luxon'
-import LfxTag from '~/components/uikit/tag/tag.vue';
 import LfxTooltip from '~/components/uikit/tooltip/tooltip.vue';
 import LfxIcon from '~/components/uikit/icon/icon.vue';
 import { useProjectStore } from '~~/app/components/modules/project/store/project.store';
 import { formatNumberShort } from '~/components/shared/utils/formatter';
 
 const { project } = storeToRefs(useProjectStore())
-const tmpShowFlag = true;
 
 const formatFirstCommit = (date: string) => {
   const dt = DateTime.fromSQL(date);
