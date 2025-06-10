c98a24e1928b48abcbb337aa77a69f9b9d61f51d
Raúl Santos <4837+borfast@users.noreply.github.com>
2025-06-10 12:47:03 +0100

    feat: implement search volume data service in Temporal using the keywords everywhere API
    Signed-off-by: Raúl Santos <4837+borfast@users.noreply.github.com>

---
+++ workers/temporal/search_volume_worker/src/activities.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2025 The Linux Foundation and each contributor.
+// SPDX-License-Identifier: MIT
+import { runMonthlySearchVolumeUpdate } from "./activities/monthlySearchVolumeUpdate";
+
+export { runMonthlySearchVolumeUpdate };
---
+++ workers/temporal/search_volume_worker/src/search-volume/types.ts
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 The Linux Foundation and each contributor.
+// SPDX-License-Identifier: MIT
+export interface ISearchVolumeParams {
+  keyword?: string;
+  limit?: number;
+  batchSize?: number;
+}
