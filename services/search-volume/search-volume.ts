aa3dddcde9e9eb16cc334695e4b21434c2242f05
Raúl Santos <4837+borfast@users.noreply.github.com>
2025-06-12 17:10:36 +0100

    fix: add SPDX license headers
    Signed-off-by: Raúl Santos <4837+borfast@users.noreply.github.com>

--- services/search-volume/search-volume.ts
+++ services/search-volume/search-volume.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2025 The Linux Foundation and each contributor.
+// SPDX-License-Identifier: MIT
 import {getSearchVolume} from './keywords-everywhere.js';
 import {closeDatabase, persistSearchVolume} from './database.js';
 // @ts-ignore This should be temporary until we have the monorepo structure set up.
