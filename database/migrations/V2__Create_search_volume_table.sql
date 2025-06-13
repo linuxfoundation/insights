cfc06dc0a3720fc93d49dbf5424e7b697b105e03
Raúl Santos <4837+borfast@users.noreply.github.com>
2025-06-13 11:56:56 +0100

    feat: add updated_at to the search volume table
    Signed-off-by: Raúl Santos <4837+borfast@users.noreply.github.com>

--- database/migrations/V2__Create_search_volume_table.sql
+++ database/migrations/V2__Create_search_volume_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE search_volume (
+CREATE TABLE IF NOT EXISTS search_volume (
     id SERIAL PRIMARY KEY,
     project_id TEXT NOT NULL,
     keyword TEXT NOT NULL,
     data_timestamp DATE NOT NULL,
-    volume INTEGER NOT NULL
+    volume INTEGER NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
 );
