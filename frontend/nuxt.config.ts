b2d95d1be1e4298ceda186e72b1ee23fcda88580
Gašper Grom <gasper.grom@gmail.com>
2025-06-16 16:26:36 +0200

    fix: extend cache to 1h
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/**': {
-      swr: 900
+      swr: 3600
     },
   }
 });
--- frontend/stress-test.js
+++ frontend/stress-test.js
@@ -44,8 +44,8 @@ const top30projects = top200Projects.slice(0, 30);
 
 export const options = {
     stages: [
-        { duration: '10s', target: 100 },
-        { duration: '20s', target: 100 },
+        { duration: '10s', target: 5 },
+        { duration: '20s', target: 5 },
         { duration: '20s', target: 0 },
     ],
 };
