f76492f857b23bbdf2a27a851111149b4c2b7124
Gašper Grom <gasper.grom@gmail.com>
2025-06-17 12:35:27 +0200

    feat: rename endpoint
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/server/api/health/live.ts
+++ frontend/server/api/health/live.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
 /**
- * API Endpoint: /api/health/kube
+ * API Endpoint: /api/health/live
  * Method: GET
  * Description: Health check used by Kubernetes to verify the status of the API.
  */
