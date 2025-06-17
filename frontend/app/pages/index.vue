ffe4caf855fdb44bba6ef3b7f169415468bf5687
emlimlf <52259294+emlimlf@users.noreply.github.com>
2025-06-17 17:24:53 +0800

    Improvement/enable explore page (#380)
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,9 +3,9 @@ Copyright (c) 2025 The Linux Foundation and each contributor.
 SPDX-License-Identifier: MIT
 -->
 <template>
-  <lfx-collection-list-view />
+  <lfx-explore />
 </template>
 
 <script setup lang="ts">
-import LfxCollectionListView from "~/components/modules/collection/views/collection-list.vue";
+import LfxExplore from '~/components/modules/explore/views/explore.vue';
 </script>
