d210441afef2f1868543eac1920e3930a272681b
Gašper Grom <gasper.grom@gmail.com>
2025-06-17 15:53:23 +0200

    fix caching
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/caching.ts
+++ frontend/setup/caching.ts
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: MIT
 export default {
     routeRules: {
-        "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
-        "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
+        // "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
+        // "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
         "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
         "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
     },
