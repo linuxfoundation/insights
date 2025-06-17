adf6f42f3c85f52a44d815385e0268c57a260bf9
Gašper Grom <gasper.grom@gmail.com>
2025-06-17 14:25:08 +0200

    fix: redis client
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/server/plugins/redis.ts
+++ frontend/server/plugins/redis.ts
@@ -1,8 +1,10 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
-// @ts-nocheck
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default defineNitroPlugin((nitroApp: any) => {
+import { defineNitroPlugin } from 'nitropack'
+import type { NitroApp } from 'nitropack'
+import { useRuntimeConfig } from '#imports'
+
+export default defineNitroPlugin((nitroApp: NitroApp) => {
     const config = useRuntimeConfig()
 
     nitroApp.storage.setItem('redis', {
