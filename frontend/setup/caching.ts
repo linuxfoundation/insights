cd36bcec491cd88c430ccd8af2a453c820997de2
Gašper Grom <gasper.grom@gmail.com>
2025-06-17 17:08:29 +0200

    fix software value on explore
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/caching.ts
+++ frontend/setup/caching.ts
@@ -1,7 +1,9 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
 export default {
-    routeRules: {
+    routeRules: process.env.NODE_ENV === 'development'
+        ? {}
+        : {
         "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
         "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
         "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
