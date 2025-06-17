81d3578b9410dd6b684b95aa5f47a884f00d5159
Efren Lim <elim@linuxfoundation.org>
2025-06-17 10:37:13 +0800

    used vue3-carousel
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/uikit/carousel/carousel.vue
+++ frontend/app/components/uikit/carousel/carousel.vue
@@ -6,78 +6,72 @@ SPDX-License-Identifier: MIT
   <client-only>
     <Carousel v-bind="carouselConfig">
       <Slide
-        v-for="slide in 10"
-        :key="slide"
+        v-for="(slide, index) in props.value"
+        :key="index"
       >
-        <div class="carousel__item w-full">
-          <div class="h-20 bg-brand-200 w-full">
-            {{ slide }}
-          </div>
+        <div class="carousel__item">
+          <slot
+            name="item"
+            :data="slide"
+          />
         </div>
       </Slide>
 
       <template #addons>
-        <Navigation />
-        <Pagination />
+        <Navigation>
+          <template #next>
+            <lfx-icon name="angle-right" />
+          </template>
+          <template #prev>
+            <lfx-icon name="angle-left" />
+          </template>
+        </Navigation>
+        <Pagination :paginate-by-items-to-show="true" />
       </template>
     </Carousel>
   </client-only>
-  <!-- <pv-carousel
-    :value="props.value"
-    :circular="props.circular"
-    :responsive-options="responsiveOptions"
-  >
-    <template #item="slotProps">
-      <slot
-        name="item"
-        :data="slotProps.data"
-      />
-    </template>
-  </pv-carousel> -->
+
+  <!-- TODO: Follow up on bug reported on the plugin:
+   https://github.com/ismail9k/vue3-carousel/issues/522 -->
 </template>
 
 <script setup lang="ts">
-// import { ref, onMounted, onUnmounted } from 'vue';
-// import type { CarouselProps } from './types/carousel.types';
+import { ref } from 'vue';
 import {
  Carousel, Slide, Pagination, Navigation
 } from 'vue3-carousel'
+import type { CarouselProps } from './types/carousel.types';
+import LfxIcon from '@/components/uikit/icon/icon.vue';
+
+const props = defineProps<CarouselProps>();
 
 const carouselConfig = {
   itemsToShow: 3,
+  itemsToScroll: 3,
+  slideAlign: 'start',
+  preventExcessiveDragging: true,
   wrapAround: true,
+  gap: 32,
   breakpoints: {
+    1280: {
+      itemsToShow: 3,
+      itemsToScroll: 3,
+    },
     1024: {
       itemsToShow: 3,
+      itemsToScroll: 3,
     },
     768: {
       itemsToShow: 2,
+      itemsToScroll: 2,
+      gap: 20,
     },
     500: {
       itemsToShow: 1,
+      itemsToScroll: 1,
     },
   }
 }
-
-// const props = defineProps<CarouselProps>();
-
-// const responsiveOptions = ref([
-//     {
-//         breakpoint: '5000px',
-//         numVisible: 3,
-//         numScroll: 3
-//     },
-//     {
-//         breakpoint: '1023px',
-//         numVisible: 2,
-//         numScroll: 2
-//     },
-//     {
-//         breakpoint: '767px',
-//         numVisible: 1,
-//         numScroll: 1
-//     }
-// ]);
 
 </script>
 
