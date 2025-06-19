ca232ddccc1b1c385c6403055b3f89f3a73f1eb3
Gašper Grom <gasper.grom@gmail.com>
2025-06-19 15:41:34 +0200

    feat: project warming up caching
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/.stress/warm-cache.js
+++ frontend/.stress/warm-cache.js
@@ -4,64 +4,73 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-const paths = ['', '/collections', '/open-source-index'];
+const baseUrl = 'https://insights.linuxfoundation.org'
+const topProjectsCount = 3
 
-const top200Projects = [
-    'k8s', 'flutter', 'ojsf-nodejs', 'helm', 'korg', 'ptproject', 'project-eve', 'Next.js', 'definitelytyped',
-    'ant-design-ant-design', 'python-cpython', 'opentelemetry', 'godotengine-godot', 'laravel-framework', 'argo',
-    'grpc', 'pandas-dev-pandas', 'angular', 'NixOS', 'prometheus', 'golang-website', 'react', 'dotnet', 'jupyter',
-    'keycloak', 'mui-material-ui', 'zep', 'odoo', 'vllm', 'opensearch-foundation', 'gql', 'ojsf-express', 'istio',
-    'cncf', 'fluentd', 'storybookjs-storybook', 'symfony-framework-bundle', 'lf-decentralized-trust',
-    'spring-projects-spring-boot', 'raspberrypi-linux', 'aws-constructs', 'easycla', 'islet', 'systemd', 'envoy',
-    'servo-llc', 'scipy', 'harbor', 'ipython', 'getsentry-sentry', 'jupyter-frontends', 'kubeflow', 'lfedge', 'px4',
-    'numpy', 'julialang-julia', 'etcd', 'fluxcd', 'lfai-onnx', 'matplotlib', 'backstage', 'cephfoundation', 'nats',
-    'cert-manager', 'osjf-fastify', 'platformio-platform-espressif32', 'jupyterhub', 'MLF', 'cilium', 'babel',
-    'ojsf-jquery', 'risc-v-international', 'homebrew-brew', 'spring-projects-spring-framework', 'cncf-distribution',
-    'model-context-protocol-mcp', 'dapr', 'test', 'quarkusio-quarkus', 'k3s', 'app-runtime-interfaces', 'opentf',
-    'spack', 'jaeger', 'expensify-app', 'containerd', 'netty', 'cdf-spinnaker', 'deepspeed', 'presto',
-    'multipath-tcp-mptcp_net-next', 'asyncapi', 'sonic-foundation', 'aswf', 'openpolicyagent', 'lfai-milvus', 'yocto',
-    'openapi', 'rook', 'fabric', 'ojsf-moment', 'onap-music', 'linkerd', 'foundational-infrastructure',
-    'cms-sw-cmssw', 'KEDA', 'onap', 'thanos', 'odl-plastic', 'mavlink', 'knative', 'deltalake', 'meshery', 'networkx',
-    'atlantis', 'connect', 'amd-rocm', 'strimzi', 'longhorn', 'typescript-eslint-typescript-eslint', 'oneapi',
-    'cdf-jenkins-x', 'perspective', 'score', 'gentoo', 'dolibarr', 'externalsecretsoperator', 'tekton', 'kyverno',
-    'kubevirt', 'exodus', 'odl-genius', 'grid', 'ojsf-electron', 'moon', 'tikv', 'coredns', 'o3de-project',
-    'scikit-image-scikit-image', 'falco', 'delta', 'opnfv-fuel', 'claude-code', 'jupyter-widgets', 'jenkins',
-    'deeprec', 'onap-holmes', 'odl-telemetry', 'admin-and-legacy-cff', 'fluid', 'notary', 'qgroundcontrol',
-    'ojsf-jqui', 'lf-decentralized-trust-labs', 'opencontainers', 'openebs', 'federatedai', 'webkit', 'cortex', 'c7n',
-    'hiero', 'Volcano', 'horovod', 'explorer', 'everest', 'hpc-toolkit', 'p4-fund', 'kubeedge', 'telepresence', 'd7y',
-    'zowe-binary-project', 'mscs', 'vitess', 'symphony-java-toolkit', 'sigstore', 'claimed', 'jupyter-server',
-    'opendaylight', 'dex', 'litmuschaos', 'kubescape', 'kserve', 'app-runtime-platform', 'valkey', 'curve', 'dpdk',
-    'openbao-2', 'emissary', 'fdio-llc', 'canon', 'flyte', 'xen', 'capsule', 'aether-project', 'crossplane',
-    'ojsf-intern', 'dronecode', 'agl', 'cobol-working-group', 'metallb',
+const basePaths = [
+    '',
+    '/collections',
+    '/open-source-index'
 ];
 
 const projectPaths = [
     '',
-    '/contributors',
-    '/popularity',
-    '/development',
     '/security',
 ];
 
-top200Projects.forEach((project) => {
-    projectPaths.forEach((path) => {
-        paths.push(`/project/${project}${path}`);
-    });
-});
+const projectPathsWithTimeRanges = [
+    '/contributors',
+    '/popularity',
+    '/development',
+];
+
+const projectTimeRanges = [
+    'past90days',
+    'past180days',
+    'past365days',
+    'previousQuarter',
+    'previousYear',
+    'previous5Year',
+    'previous10Year',
+    'alltime',
+];
+
+export function setup() {
+    const allPaths = [...basePaths];
+    // Fetch top projects before test starts
+    const projectRequest = http.get(
+        `${baseUrl}/api/project?page=0&pageSize=${topProjectsCount}&sort=score_desc&onboarded=true&isLF=true`
+    );
+    const projects = JSON.parse(projectRequest.body)?.data || [];
+    const projectSlugs = projects.map((project) => project.slug);
+
+    projectSlugs.forEach((slug) => {
+        projectPaths.forEach((path) => {
+            allPaths.push(`/project/${slug}${path}`);
+        })
+        projectPathsWithTimeRanges.forEach((path) => {
+            projectTimeRanges.forEach((timeRange) => {
+                allPaths.push(`/project/${slug}${path}?timeRange=${timeRange}`);
+            })
+        })
+    })
+
+    return allPaths;
+}
 
 export const options = {
     vus: 1,
-    iterations: paths.length,
+    iterations: 1,
 };
 
-let i = 0;
-
-export default function () {
-    const path = paths[i];
-    const url = `https://insights.linuxfoundation.org/${path}`;
-    const res = http.get(url);
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-    });
-    i += 1;
+export default function (paths) {
+    paths.forEach((path) => {
+        const url = `${baseUrl}${path}`;
+        const res = http.get(url);
+        console.log(`Warmed up: ${url}`);
+        check(res, {
+            'status is 200': (r) => r.status === 200,
+        });
+        sleep(0.5);
+    })
 }
