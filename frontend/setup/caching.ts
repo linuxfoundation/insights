421ae21329918090a24c900a3af33de730e109f1
Gašper Grom <gasper.grom@gmail.com>
2025-06-19 17:55:21 +0200

    fix: cache config (#432)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/caching.ts
+++ frontend/setup/caching.ts
@@ -12,10 +12,10 @@ export default {
         // "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
         // "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
         // "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
-        "/api/search": {swc: 86400}, // Cache search results for 1 day
-        "/api/**": {swc: 3600}, // Cache API responses for 1 hour
-        "/project/**": {swc: 3600}, // Cache project details for 1 hour
-        "**": {swc: 86400}, // Cache all other routes for 1 day
+        "/api/search": {swr: 86400}, // Cache search results for 1 day
+        "/api/**": {swr: 3600}, // Cache API responses for 1 hour
+        "/project/**": {swr: 3600}, // Cache project details for 1 hour
+        "**": {swr: 86400}, // Cache all other routes for 1 day
     },
     // nitro: {
     //     storage: {
