626105838692daa66cad35c28d14942a42779489
Gašper Grom <gasper.grom@gmail.com>
2025-06-20 13:32:11 +0200

    feat: enable redis
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/caching.ts
+++ frontend/setup/caching.ts
@@ -8,21 +8,21 @@ export default {
         "/api/seo/og-image": {cache: false},
         "/api/report": {cache: false},
         "/api/health/live": {cache: false},
-        // "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
-        // "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
-        // "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
-        // "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
-        "/api/search": {swr: 86400}, // Cache search results for 1 day
-        "/api/**": {swr: 3600}, // Cache API responses for 1 hour
-        "/project/**": {swr: 3600}, // Cache project details for 1 hour
-        "**": {swr: 86400}, // Cache all other routes for 1 day
+        "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
+        "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
+        "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
+        "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
+        // "/api/search": {swr: 86400}, // Cache search results for 1 day
+        // "/api/**": {swr: 3600}, // Cache API responses for 1 hour
+        // "/project/**": {swr: 3600}, // Cache project details for 1 hour
+        // "**": {swr: 86400}, // Cache all other routes for 1 day
     },
-    // nitro: {
-    //     storage: {
-    //         redis: {
-    //             driver: 'redis',
-    //             url: process.env.NUXT_REDIS_URL || '',
-    //         },
-    //     },
-    // },
+    nitro: {
+        storage: {
+            redis: {
+                driver: 'redis',
+                url: process.env.NUXT_REDIS_URL || '',
+            },
+        },
+    },
 }
