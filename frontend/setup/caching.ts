fa73526fbd5678d4ff8dcf2174a0b16b8097ed8a
Gašper Grom <gasper.grom@gmail.com>
2025-06-20 14:17:23 +0200

    Cache only api requests
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/setup/caching.ts
+++ frontend/setup/caching.ts
@@ -10,8 +10,9 @@ export default {
         "/api/health/live": {cache: false},
         "/api/search": {cache: {maxAge: 86400, base: 'redis'}}, // Cache search results for 1 day
         "/api/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache API responses for 1 hour
-        "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
-        "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
+        // "/project/**": {cache: {maxAge: 3600, base: 'redis'}}, // Cache project details for 1 hour
+        // "**": {cache: {maxAge: 86400, base: 'redis'}}, // Cache all other routes for 1 day
+        "**": {cache: false}, // Cache all other routes for 1 day
         // "/api/search": {swr: 86400}, // Cache search results for 1 day
         // "/api/**": {swr: 3600}, // Cache API responses for 1 hour
         // "/project/**": {swr: 3600}, // Cache project details for 1 hour
