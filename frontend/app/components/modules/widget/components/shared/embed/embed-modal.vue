e187a9eba76e49510a363df3eaece71018f0ef56
Gašper Grom <gasper.grom@gmail.com>
2025-08-04 13:59:27 +0200

    fix: embed not working (#570)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/modules/widget/components/shared/embed/embed-modal.vue
+++ frontend/app/components/modules/widget/components/shared/embed/embed-modal.vue
@@ -142,18 +142,14 @@ const isModalOpen = computed<boolean>({
   set: (value: boolean) => emit('update:modelValue', value)
 });
 const {
-project, repository, startDate, endDate, selectedTimeRangeKey
+project, selectedRepositories, startDate, endDate, selectedTimeRangeKey
 } = storeToRefs(useProjectStore())
 
 const widgetConfig = computed(() => lfxWidgets[props.widgetName]);
 
 const domain = computed(() => window?.location.origin);
 const baseUrl = computed(() => {
-  const url = `${domain.value}/embed/project/${project.value?.slug}`;
-  if(repository.value?.slug) {
-    return `${url}/repository/${repository.value?.slug}`;
-  }
-  return url
+  return `${domain.value}/embed/project/${project.value?.slug}`
 });
 
 const queryParams = computed(() => {
@@ -162,6 +158,9 @@ const queryParams = computed(() => {
   params.set('startDate', startDate.value || '');
   params.set('endDate', endDate.value || '');
   params.set('timeRangeKey', selectedTimeRangeKey.value || '');
+  if(selectedRepositories.value?.length > 0) {
+    params.set('repos', selectedRepositories.value.map((repo) => repo.slug).join('|'));
+  }
   Object.entries(props.data).forEach(([key, value]) => {
     if(value !== undefined && value !== null) {
       params.set(key, String(value));
