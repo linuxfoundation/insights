aef4ba646eb5f589262cd13d62c88c3a6e8d14f4
anilb <epipav@gmail.com>
2025-08-20 15:34:11 +0200

    fix: better checks for local
    Signed-off-by: anilb <epipav@gmail.com>

--- frontend/server/middleware/jwt-auth.ts
+++ frontend/server/middleware/jwt-auth.ts
@@ -33,9 +33,10 @@ export default defineEventHandler(async (event) => {
 
   const token = authHeader.substring(7);
   const auth0Domain = config.public.auth0Domain;
-  const auth0Audience = config.public.auth0ClientId;
+  const auth0ClientId = config.public.auth0ClientId;
+  const auth0Audience = config.public.auth0Audience;
 
-  if (!auth0Domain || !auth0Audience) {
+  if (!auth0Domain || !auth0ClientId) {
     throw createError({
       statusCode: 500, 
       statusMessage: 'Auth0 configuration missing'
@@ -49,7 +50,7 @@ export default defineEventHandler(async (event) => {
       
       const { payload } = await jwtVerify(token, jwks, {
         issuer: `https://${auth0Domain}/`,
-        audience: auth0Audience,
+        audience: auth0ClientId,
       });
       
       event.context.user = payload;
@@ -57,7 +58,7 @@ export default defineEventHandler(async (event) => {
       const GROUP_CLAIM_KEY = 'https://sso.linuxfoundation.org/claims/groups'
       const GROUP_NAME = 'lfproducts-lfx-insights'
 
-      if (auth0Domain != 'localhost' && !(payload[GROUP_CLAIM_KEY] as string[]).includes(GROUP_NAME)) {
+      if (!auth0Audience.includes('localhost') && !(payload[GROUP_CLAIM_KEY] as string[]).includes(GROUP_NAME)) {
         throw createError({
           statusCode: 401,
           statusMessage: `User does not belong to ${GROUP_NAME}`
