3c1bbe2c08ee44fc0419b9906586f276f9e4a861
Gašper Grom <gasper.grom@gmail.com>
2025-08-28 09:38:41 +0200

    feat: kubecon banner (#611)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/components/shared/layout/navbar.vue
+++ frontend/app/components/shared/layout/navbar.vue
@@ -4,6 +4,17 @@ SPDX-License-Identifier: MIT
 -->
 <template>
   <header class="outline outline-1 outline-neutral-100 bg-white fixed top-0 left-0 z-40 w-full">
+
+    <!-- Banner -->
+    <div class="block bg-brand-500 text-white text-center h-14 sm:h-7 text-body-1 leading-7">
+      Meet us in Atlanta for KubeCon + CloudNativeCon North America · Nov 10-13 · <a
+        href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/register/?utm_source=lfx-insights&utm_medium=homepage&utm_campaign=KubeCon-NA-2025&utm_content=hero"
+        class="underline hover:no-underline whitespace-nowrap"
+        target="_blank"
+        rel="noopener"
+      >REGISTER TODAY</a>
+    </div>
+
     <div class="container">
       <div class="h-14 lg:h-17 flex justify-between items-center gap-4 lg:gap-5">
         <div class="flex items-center gap-6 flex-grow min-w-0">
