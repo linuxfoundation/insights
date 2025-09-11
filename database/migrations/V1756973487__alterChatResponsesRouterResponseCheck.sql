689508985e4980a33b4ac1e684bacb22c969e6d9
anilb <epipav@gmail.com>
2025-09-11 10:43:27 +0200

    chore: check alter migration updates old enum keys before adding the new one
    Signed-off-by: anilb <epipav@gmail.com>

--- database/migrations/V1756973487__alterChatResponsesRouterResponseCheck.sql
+++ database/migrations/V1756973487__alterChatResponsesRouterResponseCheck.sql
@@ -1,6 +1,9 @@
 -- Drop the existing check constraint
 ALTER TABLE chat_responses DROP CONSTRAINT chat_responses_router_response_check;
 
+UPDATE chat_responses SET router_response = 'create_query' 
+WHERE router_response = 'text-to-sql';
+
 -- Add the new check constraint with 'create_query' instead of 'text-to-sql'
 ALTER TABLE chat_responses ADD CONSTRAINT chat_responses_router_response_check 
     CHECK (router_response IN ('pipes', 'create_query', 'stop'));
