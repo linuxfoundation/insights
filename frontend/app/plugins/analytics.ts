5e4889de381a98a0407412fb3f0749f3a77f56ed
Gašper Grom <gasper.grom@gmail.com>
2025-09-19 12:49:51 +0200

    fix: remove console log
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/plugins/analytics.ts
+++ frontend/app/plugins/analytics.ts
@@ -3,16 +3,12 @@
 import type { NuxtApp } from 'nuxt/app'
 import {defineNuxtPlugin, useRuntimeConfig} from 'nuxt/app'
 
-const isProduction = process.env.NUXT_APP_ENV === 'production'
 export default defineNuxtPlugin(async (nuxtApp: NuxtApp) => {
-  console.log(process.server);
-  console.log(process.env.NODE_ENV, isProduction);
   if (process.server) return
-  if (process.env.NODE_ENV !== 'production' || !isProduction) return
+  if (process.env.NODE_ENV !== 'production') return
 
   const config = useRuntimeConfig()
   const cdnUrl = config.public.lfxSegmentCdnUrl as string
-  console.log(cdnUrl);
 
   if(!cdnUrl) return;
 
