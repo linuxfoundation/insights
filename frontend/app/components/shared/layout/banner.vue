af8acf398fbbab0cc362cecdabdf6871fbc52497
Efren Lim <elim@linuxfoundation.org>
2025-10-06 17:43:50 +0800

    chore: lint issues
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/shared/layout/banner.vue
+++ frontend/app/components/shared/layout/banner.vue
@@ -7,7 +7,10 @@ SPDX-License-Identifier: MIT
     class="block bg-brand-800 text-white sm:py-1 py-2 px-5 flex 
     items-center sm:justify-center justify-start gap-3"
   >
-    <span class="text-2xs sm:text-xs leading-3.5 sm:leading-4">Meet us in Atlanta for KubeCon + CloudNativeCon North America · Nov 10-13</span><a
+    <span class="text-2xs sm:text-xs leading-3.5 sm:leading-4">
+      Meet us in Atlanta for KubeCon + CloudNativeCon North America · Nov 10-13
+    </span>
+    <a
       href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/register/?utm_source=lfx-insights&utm_medium=homepage&utm_campaign=KubeCon-NA-2025&utm_content=hero"
       class="font-bold no-underline whitespace-nowrap text-2xs sm:text-xs leading-3.5 sm:leading-4"
       target="_blank"
