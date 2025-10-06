8268c99a5961d6c0ada544da26581b6f91e2421a
Efren Lim <elim@linuxfoundation.org>
2025-10-06 19:32:57 +0800

    fix: banner link hover style
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/shared/layout/banner.vue
+++ frontend/app/components/shared/layout/banner.vue
@@ -12,7 +12,7 @@ SPDX-License-Identifier: MIT
     </span>
     <a
       href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/register/?utm_source=lfx-insights&utm_medium=homepage&utm_campaign=KubeCon-NA-2025&utm_content=hero"
-      class="font-bold no-underline whitespace-nowrap sm:!text-xs text-2xs sm:leading-4 leading-3.5"
+      class="font-bold hover:underline no-underline whitespace-nowrap sm:!text-xs text-2xs sm:leading-4 leading-3.5"
       target="_blank"
       rel="noopener"
     >REGISTER TODAY</a>
