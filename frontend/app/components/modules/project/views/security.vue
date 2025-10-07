15fd084fa92e9b66103e186ec2b664a7152242e0
Efren Lim <elim@linuxfoundation.org>
2025-10-07 15:58:24 +0800

    fix: combine duplicate assessments
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/views/security.vue
+++ frontend/app/components/modules/project/views/security.vue
@@ -220,6 +220,7 @@ const groupedData = computed(() => (securityAssessmentData.value || []).reduce((
     if (!obj[check.category]) {
       obj[check.category] = [];
     }
+    check.assessments = PROJECT_SECURITY_SERVICE.mergeDuplicateAssessments(check.assessments);
     obj[check.category]?.push(check);
     return obj;
   }, {} as Record<string, SecurityData[]>))
@@ -229,6 +230,7 @@ const groupChecksByRepository = (checks: SecurityData[]) => (checks || []).reduc
     if (!obj[check.repo]) {
       obj[check.repo] = [];
     }
+    check.assessments = PROJECT_SECURITY_SERVICE.mergeDuplicateAssessments(check.assessments);
     obj[check.repo]?.push(check);
     return obj;
   }, {} as Record<string, SecurityData[]>)
