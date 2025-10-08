7c737fcb38d0738cc7d77c359dca692f0816aff5
Efren Lim <elim@linuxfoundation.org>
2025-10-08 10:36:52 +0800

    feat: add dynamic activity type filtering when toggling collaborations
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/config/platforms/configs/jira.platform.ts
+++ frontend/app/config/platforms/configs/jira.platform.ts
@@ -1,8 +1,8 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
-import { ActivityPlatforms } from '~~/types/shared/activity-platforms';
-import { ActivityTypes } from '~~/types/shared/activity-types';
-import type { PlatformConfig } from '~~/types/shared/platforms.types';
+import { ActivityPlatforms } from '~~/types/shared/activity-platforms'
+import { ActivityTypes } from '~~/types/shared/activity-types'
+import type { PlatformConfig } from '~~/types/shared/platforms.types'
 
 export const jira: PlatformConfig = {
   key: ActivityPlatforms.JIRA,
@@ -11,31 +11,38 @@ export const jira: PlatformConfig = {
   activityTypes: [
     {
       key: ActivityTypes.ISSUE_CREATED,
-      label: 'Created an issue'
+      label: 'Created an issue',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUES_CLOSED,
-      label: 'Closed an issue'
+      label: 'Closed an issue',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUE_ASSIGNED,
-      label: 'Assigned an issue'
+      label: 'Assigned an issue',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUE_UPDATED,
-      label: 'Updated an issue'
+      label: 'Updated an issue',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUE_COMMENT_CREATED,
-      label: 'Created an issue comment'
+      label: 'Created an issue comment',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUE_COMMENT_UPDATED,
-      label: 'Updated an issue comment'
+      label: 'Updated an issue comment',
+      isCollaborationType: true,
     },
     {
       key: ActivityTypes.ISSUE_ATTACHMENT_ADDED,
-      label: 'Added an attachment to an issue'
-    }
-  ]
-};
+      label: 'Added an attachment to an issue',
+      isCollaborationType: true,
+    },
+  ],
+}
