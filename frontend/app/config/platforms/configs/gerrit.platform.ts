facd854b7bd77d2389fe0100d45f310a7a1236a6
Raúl Santos <4837+borfast@users.noreply.github.com>
2025-10-09 16:51:04 +0100

    feat: hide activity types that don't exist for a project
    Signed-off-by: Raúl Santos <4837+borfast@users.noreply.github.com>
    
    feat: provide a new endpoint with available activity types
    
    Signed-off-by: Raúl Santos <4837+borfast@users.noreply.github.com>

--- frontend/app/config/platforms/configs/gerrit.platform.ts
+++ frontend/app/config/platforms/configs/gerrit.platform.ts
@@ -1,45 +1,10 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
 import { ActivityPlatforms } from '~~/types/shared/activity-platforms';
-import { ActivityTypes } from '~~/types/shared/activity-types';
 import type { PlatformConfig } from '~~/types/shared/platforms.types';
 
 export const gerrit: PlatformConfig = {
   key: ActivityPlatforms.GERRIT,
   label: 'Gerrit',
   image: '/images/integrations/gerrit.png',
-  activityTypes: [
-    {
-      key: ActivityTypes.CHANGESET_CREATED,
-      label: 'Created a changeset'
-    },
-    {
-      key: ActivityTypes.CHANGESET_MERGED,
-      label: 'Merged a changeset'
-    },
-    {
-      key: ActivityTypes.CHANGESET_CLOSED,
-      label: 'Closed a changeset'
-    },
-    {
-      key: ActivityTypes.CHANGESET_ABANDONED,
-      label: 'Abandoned a changeset'
-    },
-    {
-      key: ActivityTypes.CHANGESET_COMMENT_CREATED,
-      label: 'Created a changeset comment'
-    },
-    {
-      key: ActivityTypes.PATCHSET_CREATED,
-      label: 'Created a patchset'
-    },
-    {
-      key: ActivityTypes.PATCHSET_COMMENT_CREATED,
-      label: 'Created a patchset comment'
-    },
-    {
-      key: ActivityTypes.PATCHSET_APPROVAL_CREATED,
-      label: 'Created a patchset approval'
-    }
-  ]
 };
--- frontend/app/config/platforms/configs/git.platform.ts
+++ frontend/app/config/platforms/configs/git.platform.ts
@@ -1,57 +1,10 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
 import { ActivityPlatforms } from '~~/types/shared/activity-platforms';
-import { ActivityTypes } from '~~/types/shared/activity-types';
 import type { PlatformConfig } from '~~/types/shared/platforms.types';
 
 export const git: PlatformConfig = {
   key: ActivityPlatforms.GIT,
   label: 'Git',
   image: '/images/integrations/git.png',
-  activityTypes: [
-    {
-      key: ActivityTypes.AUTHORED_COMMIT,
-      label: 'Authored a commit'
-    },
-    {
-      key: ActivityTypes.REVIEWED_COMMIT,
-      label: 'Reviewed a commit'
-    },
-    {
-      key: ActivityTypes.TESTED_COMMIT,
-      label: 'Tested a commit'
-    },
-    {
-      key: ActivityTypes.COAUTHORED_COMMIT,
-      label: 'Co-authored a commit'
-    },
-    {
-      key: ActivityTypes.INFORMED_COMMIT,
-      label: 'Informed a commit'
-    },
-    {
-      key: ActivityTypes.INFLUENCED_COMMIT,
-      label: 'Influenced a commit'
-    },
-    {
-      key: ActivityTypes.APPROVED_COMMIT,
-      label: 'Approved a commit'
-    },
-    {
-      key: ActivityTypes.COMMITTED_COMMIT,
-      label: 'Committed a commit'
-    },
-    {
-      key: ActivityTypes.REPORTED_COMMIT,
-      label: 'Reported a commit'
-    },
-    {
-      key: ActivityTypes.RESOLVED_COMMIT,
-      label: 'Resolved a commit'
-    },
-    {
-      key: ActivityTypes.SIGNEDOFF_COMMIT,
-      label: 'Signed off a commit'
-    }
-  ]
 };
