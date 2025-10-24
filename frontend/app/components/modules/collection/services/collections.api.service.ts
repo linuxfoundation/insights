1f483d4c642987e735277a1e1f0f713bc4b8d9b8
Efren Lim <elim@linuxfoundation.org>
2025-10-24 15:27:37 +0800

    chore: refactor the api calls in the collections list and the collection page
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/collection/services/collections.api.service.ts
+++ frontend/app/components/modules/collection/services/collections.api.service.ts
@@ -1,47 +1,131 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
-import type { QueryFunction } from '@tanstack/vue-query';
-import type { Pagination } from '~~/types/shared/pagination';
-import type { Collection } from '~~/types/collection';
-import type { Category, CategoryGroup } from '~~/types/category';
+import { type QueryFunction, useInfiniteQuery, useQuery, useQueryClient } from '@tanstack/vue-query'
+import { type ComputedRef, computed } from 'vue'
+import type { Pagination } from '~~/types/shared/pagination'
+import type { Collection } from '~~/types/collection'
+import type { Category, CategoryGroup } from '~~/types/category'
+import { TanstackKey } from '~/components/shared/types/tanstack'
 
 export interface CategoryGroupOptions {
-  value: string;
-  categories: Category[];
-  id: string;
-  name: string;
+  value: string
+  categories: Category[]
+  id: string
+  name: string
 }
+
+export interface QueryParams {
+  pageSize: number
+  sort: string
+  categories: string
+}
+
+export interface CategoryGroupsQueryParams {
+  type: 'vertical' | 'horizontal'
+  pageSize: number
+}
+
 class CollectionsApiService {
-  fetchCollectionList(
-      query: Record<string, string | number | string[] | undefined>
-  ): Promise<Pagination<Collection>> {
-    return $fetch('/api/collection', {
-      params: query,
-    });
+  async prefetchCollections(params: ComputedRef<QueryParams>) {
+    const queryClient = useQueryClient()
+    const queryKey = computed(() => [
+      TanstackKey.COLLECTIONS,
+      params.value.sort,
+      params.value.categories,
+    ])
+
+    const queryFn = this.fetchCollectionsQueryFn(() => ({
+      ...params.value,
+    }))
+
+    return await queryClient.prefetchInfiniteQuery<
+      Pagination<Collection>,
+      Error,
+      Pagination<Collection>,
+      readonly unknown[],
+      number
+    >({
+      queryKey,
+      queryFn,
+      initialPageParam: 0,
+      getNextPageParam: this.getNextPageCollectionsParam,
+    })
   }
 
-  fetchCollections(
-    query: () => Record<string, string | number | string[] | undefined>
-  ): QueryFunction<Pagination<Collection>> {
-    return async ({ pageParam = 0 }) => await $fetch('/api/collection', {
+  getNextPageCollectionsParam(lastPage: Pagination<Collection>) {
+    const nextPage = lastPage.page + 1
+    const totalPages = Math.ceil(lastPage.total / lastPage.pageSize)
+    return nextPage < totalPages ? nextPage : null
+  }
+
+  fetchCollections(params: ComputedRef<QueryParams>) {
+    const queryKey = computed(() => [
+      TanstackKey.COLLECTIONS,
+      params.value.sort,
+      params.value.categories,
+    ])
+
+    const queryFn = this.fetchCollectionsQueryFn(() => ({
+      ...params.value,
+    }))
+
+    return useInfiniteQuery<
+      Pagination<Collection>,
+      Error,
+      Pagination<Collection>,
+      readonly unknown[],
+      number
+    >({
+      queryKey,
+      queryFn,
+      getNextPageParam: this.getNextPageCollectionsParam,
+      initialPageParam: 0,
+    })
+  }
+
+  fetchCollectionsQueryFn(
+    query: () => Record<string, string | number | string[] | undefined>,
+  ): QueryFunction<Pagination<Collection>, readonly unknown[], number> {
+    return async ({ pageParam = 0 }) =>
+      await $fetch('/api/collection', {
         params: {
           page: pageParam,
           ...query(),
         },
-      });
+      })
   }
 
   fetchCollection(slug: string): QueryFunction<Collection> {
-    return () => $fetch(`/api/collection/${slug}`);
+    return () => $fetch(`/api/collection/${slug}`)
   }
 
-  fetchCategoryGroups(
-    query: () => Record<string, string | number>
+  fetchCategoryGroups(params: ComputedRef<CategoryGroupsQueryParams>) {
+    const queryKey = computed(() => [
+      TanstackKey.CATEGORY_GROUPS,
+      params.value.type,
+      params.value.pageSize,
+    ])
+
+    const queryFn = computed<QueryFunction<Pagination<CategoryGroup>>>(() =>
+      this.fetchCategoryGroupsQueryFn(() => ({
+        ...params.value,
+      })),
+    )
+
+    return useQuery<Pagination<CategoryGroup>, Error>({
+      queryKey,
+      queryFn,
+    })
+  }
+
+  fetchCategoryGroupsQueryFn(
+    query: () => Record<string, string | number>,
   ): QueryFunction<Pagination<CategoryGroup>> {
-    return () => $fetch(`/api/category`, {
+    return () =>
+      $fetch(`/api/category`, {
         params: query(),
-      });
+      })
   }
 }
 
-export const COLLECTIONS_API_SERVICE = new CollectionsApiService();
+export const COLLECTIONS_API_SERVICE = new CollectionsApiService()
