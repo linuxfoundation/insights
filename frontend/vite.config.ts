3e0e3599144fb52ca89bd43d25e76d2a9c5fd89d
Gašper Grom <gasper.grom@gmail.com>
2026-01-20 11:50:38 +0100

    feat: optimize running of development server (#1602)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025 The Linux Foundation and each contributor.
-// SPDX-License-Identifier: MIT
-import { defineConfig } from 'vitepress';
-
-export default defineConfig({
-  vite: {
-    root: __dirname,
-  },
-});
