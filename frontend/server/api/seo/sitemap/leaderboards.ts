87c11a42b495cd98cd572eb8df99d5d74e442c5e
Gašper Grom <gasper.grom@gmail.com>
2026-01-21 09:01:29 +0100

    fix: sitemap generation (#1609)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/server/api/seo/sitemap/leaderboards.ts
+++ frontend/server/api/seo/sitemap/leaderboards.ts
@@ -1,10 +1,14 @@
 // Copyright (c) 2025 The Linux Foundation and each contributor.
 // SPDX-License-Identifier: MIT
 
-import leaderboardConfigs from '~/components/modules/leaderboards/config/index.config';
+import { fetchFromTinybird } from '~~/server/data/tinybird/tinybird';
 
 export default defineSitemapEventHandler(async () => {
-  return leaderboardConfigs.map((config) => ({
-    loc: `/leaderboards/${config.key}`,
+  const res = await fetchFromTinybird<{ leaderboardType: string }[]>(
+    '/v0/pipes/sitemap_leaderboards.json',
+    {},
+  );
+  return res.data.map((item) => ({
+    loc: `/leaderboards/${item.leaderboardType}`,
   }));
 });
