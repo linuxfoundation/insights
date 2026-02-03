70f51a0a213df12291a73fd1cd313bdfe757c9f7
Efren Lim <elim@linuxfoundation.org>
2026-02-03 15:44:17 +0800

    fix: link no referrer issue
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/components/modules/project/components/overview/score-details/score-tab-view.vue
+++ frontend/app/components/modules/project/components/overview/score-details/score-tab-view.vue
@@ -22,6 +22,7 @@ SPDX-License-Identifier: MIT
               <a
                 :href="links.securityScore"
                 target="_blank"
+                rel="noopener noreferrer"
                 class="text-brand-500"
                 >Learn more</a
               >
