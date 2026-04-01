212cdd2c609c02b5ec6a70fa13e9bfae1df66ce0
Efren Lim <elim@linuxfoundation.org>
2026-04-01 13:44:59 +0800

    chore: address PR comments
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/app/config/advisory-databases.ts
+++ frontend/app/config/advisory-databases.ts
@@ -3,34 +3,28 @@
 
 export interface AdvisoryDatabase {
   prefix: string;
-  baseUrl: string;
   buildUrl: (id: string) => string;
 }
 
 export const advisoryDatabases: AdvisoryDatabase[] = [
   {
     prefix: 'CVE',
-    baseUrl: 'https://nvd.nist.gov/vuln/detail',
     buildUrl: (id: string) => `https://nvd.nist.gov/vuln/detail/${id}`,
   },
   {
     prefix: 'GHSA',
-    baseUrl: 'https://github.com/advisories',
     buildUrl: (id: string) => `https://github.com/advisories/${id}`,
   },
   {
     prefix: 'GO',
-    baseUrl: 'https://pkg.go.dev/vuln',
     buildUrl: (id: string) => `https://pkg.go.dev/vuln/${id}`,
   },
   {
     prefix: 'PYSEC',
-    baseUrl: 'https://osv.dev/vulnerability',
     buildUrl: (id: string) => `https://osv.dev/vulnerability/${id}`,
   },
   {
     prefix: 'RUSTSEC',
-    baseUrl: 'https://rustsec.org/advisories',
     buildUrl: (id: string) => `https://rustsec.org/advisories/${id}.html`,
   },
 ];
