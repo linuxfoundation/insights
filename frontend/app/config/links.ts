28eef1759bc06d16494db3ccdb9987b54d33b2be
Gašper Grom <gasper.grom@gmail.com>
2026-04-21 16:13:35 +0100

    chore: deprecate criticality banner IN-1097 (#1853)
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/app/config/links.ts
+++ frontend/app/config/links.ts
@@ -5,7 +5,6 @@ export const links = {
   trustScore: `/docs/metrics/health-score`,
   securityScore: `/docs/metrics/security`,
   ospsScore: 'https://baseline.openssf.org/versions/2025-02-25',
-  criticality: '/docs/introduction/lf-oss-index/#criticality-score',
   issues: 'https://github.com/linuxfoundation/insights/issues',
   discussions: 'https://github.com/linuxfoundation/insights/discussions',
   copilotDocs: '/docs/introduction/what-is-insights/',
