7124a1cf3aacca77f93bb511c3fdd0ac92b86527
Joana Maia <jmaia@contractor.linuxfoundation.org>
2026-05-22 17:17:03 +0100

    fix: render link with empty param (#1933)
    Signed-off-by: Joana Maia <jmaia@contractor.linuxfoundation.org>

--- frontend/app/components/modules/leaderboards/components/minimize-row-displays/organization-row.vue
+++ frontend/app/components/modules/leaderboards/components/minimize-row-displays/organization-row.vue
@@ -7,10 +7,10 @@ SPDX-License-Identifier: MIT
   <div class="flex items-center w-full">
     <!-- Organization info -->
     <component
-      :is="isTeamMember ? nuxtLink : 'div'"
-      :to="isTeamMember ? { name: LfxRoutes.ORGANIZATION, params: { orgSlug: item.slug } } : undefined"
+      :is="isTeamMember && item.slug ? nuxtLink : 'div'"
+      :to="isTeamMember && item.slug ? { name: LfxRoutes.ORGANIZATION, params: { orgSlug: item.slug } } : undefined"
       class="flex-1 min-w-0 flex gap-3 items-center text-inherit no-underline"
-      :class="isTeamMember ? 'hover:text-brand-500 transition-colors cursor-pointer' : ''"
+      :class="isTeamMember && item.slug ? 'hover:text-brand-500 transition-colors cursor-pointer' : ''"
     >
       <lfx-avatar
         :src="item.logoUrl"
