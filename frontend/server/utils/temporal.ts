6f241b04ad674edd8d1f6efab1f64a5b8cd12176
Gašper Grom <gasper.grom@gmail.com>
2026-09-18 17:03:27 +0200

    feat: add docs readiness update endpoint IN-1329
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/server/utils/temporal.ts
+++ frontend/server/utils/temporal.ts
@@ -173,3 +173,10 @@ export interface IUpsertOSPSBaselineSecurityInsightsParams {
   repoUrl: string;
   token: string;
 }
+
+export const DOCS_READINESS_TASK_QUEUE = 'docs-readiness';
+export const PROCESS_PROJECT_DOCS_READINESS_WORKFLOW = 'processProjectDocsReadiness';
+
+export interface IProcessProjectDocsReadinessParams {
+  projectId: string;
+}
